0a7152a91550d63815235e1086be09dd58c75e4e
Brian C. Van Essen <vanessen1@llnl.gov>
2017-03-31 18:18:43 -0700

    Fixed type in data set name

--- Pilot2/common/pilot2_datasets.py
+++ Pilot2/common/pilot2_datasets.py
@@ -4,5 +4,5 @@ data_sets = {
   '3k_Ordered_and_gel' : ('3k_run43_10us.35fs-DPPC.70-DOPC.10-CHOL.20.dir', '45b9a2f7deefb8d5b016b1c42f5fba71'),
   '6k_Disordered' : ('6k_run10_25us.35fs-DPPC.10-DOPC.70-CHOL.20.dir', '24e4f8d3e32569e8bdd2252f7259a65b'),
   '6k_Ordered' : ('6k_run32_25us.35fs-DPPC.50-DOPC.10-CHOL.40.dir', '0b3b39086f720f73ce52d5b07682570d'),
-  '6k_Ordered and gel' : ('6k_run43_25us.35fs-DPPC.70-DOPC.10-CHOL.20.dir', '3b3e069a7c55a4ddf805f5b898d6b1d1')
+  '6k_Ordered_and_gel' : ('6k_run43_25us.35fs-DPPC.70-DOPC.10-CHOL.20.dir', '3b3e069a7c55a4ddf805f5b898d6b1d1')
   }
