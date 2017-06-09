ca5e7ee5c927f0c60e06bc586b75f8cb0a4722d6
Harry Yoo <hsyoo@anl.gov>
2017-06-09 14:08:37 -0500

    save json file in addition to sending to solr database

--- common/solr_keras.py
+++ common/solr_keras.py
@@ -1,42 +1,55 @@
+import json
+import os
+import warnings
 from datetime import datetime
 
 import requests
-import warnings
 from keras.callbacks import Callback
 
 
 class CandleRemoteMonitor(Callback):
+    """Capture Run level output and store/send for monitoring
+    """
+
     def __init__(self,
-                 params=None,
-                 root='http://localhost:8983/solr',
-                 path='/run/update?commit=true',
-                 headers=None):
+                 params=None):
         super(CandleRemoteMonitor, self).__init__()
-        if headers is None:
-            headers = {'Content-Type': 'application/json'}
-        self.gParams = params
-        self.root = root
-        self.path = path
-        self.headers = headers
+
+        self.global_params = params
+        self.has_solr_config = False
+        if 'solr_root' in params:
+            self.has_solr_config = True
+            self.root = params['solr_root']
+            self.path = '/run/update?commit=true'
+            self.headers = {'Content-Type': 'application/json'}
+
+        # init
+        self.experiment_id = None
+        self.run_id = None
+        self.run_timestamp = None
+        self.epoch_timestamp = None
+        self.log_messages = []
 
     def on_train_begin(self, logs=None):
         logs = logs or {}
         self.run_timestamp = datetime.now()
-        self.experiment_id = self.gParams['experiment_id']
-        self.run_id = self.gParams['run_id']
+        self.experiment_id = self.global_params['experiment_id']
+        self.run_id = self.global_params['run_id']
 
-        params = []
-        for k, v in self.gParams.items():
-            params.append("{}: {}".format(k, v))
+        run_params = []
+        for key, val in self.global_params.items():
+            run_params.append("{}: {}".format(key, val))
 
-        send = [{'experiment_id': self.experiment_id,
-                 'run_id': self.run_id,
-                 'parameters': params,
-                 'start_time': 'NOW',
-                 'status': 'Started'
-                }]
+        send = {'experiment_id': self.experiment_id,
+                'run_id': self.run_id,
+                'parameters': run_params,
+                'start_time': 'NOW',
+                'status': 'Started'
+               }
         # print("on_train_begin", send)
-        self.submit(send)
+        self.log_messages.append(send)
+        if self.has_solr_config:
+            self.submit(send)
 
     def on_epoch_begin(self, epoch, logs=None):
         self.epoch_timestamp = datetime.now()
@@ -45,40 +58,59 @@ class CandleRemoteMonitor(Callback):
         logs = logs or {}
         loss = logs.get('loss')
         val_loss = logs.get('val_loss')
-        epoch_total = self.gParams['epochs']
+        epoch_total = self.global_params['epochs']
         epoch_duration = datetime.now() - self.epoch_timestamp
         epoch_in_sec = epoch_duration.total_seconds()
         epoch_line = "epoch: {}/{}, duration: {}s, loss: {}, val_loss: {}".format(
             (epoch + 1), epoch_total, epoch_in_sec, loss, val_loss)
 
-        send = [{'run_id': self.run_id,
-                 'status': {'set': 'Running'},
-                 'training_loss': {'set': loss},
-                 'validation_loss': {'set': val_loss},
-                 'run_progress': {'add': [epoch_line]}
-                }]
+        send = {'run_id': self.run_id,
+                'status': {'set': 'Running'},
+                'training_loss': {'set': loss},
+                'validation_loss': {'set': val_loss},
+                'run_progress': {'add': [epoch_line]}
+               }
         # print("on_epoch_end", send)
-        self.submit(send)
+        self.log_messages.append(send)
+        if self.has_solr_config:
+            self.submit(send)
 
     def on_train_end(self, logs=None):
         logs = logs or {}
         run_duration = datetime.now() - self.run_timestamp
         run_in_hour = run_duration.total_seconds() / (60 * 60)
 
-        send = [{'run_id': self.run_id,
-                 'runtime_hours': {'set': run_in_hour},
-                 'end_time': {'set': 'NOW'},
-                 'status': {'set': 'Finished'},
-                 'date_modified': {'set': 'NOW'}
-                }]
+        send = {'run_id': self.run_id,
+                'runtime_hours': {'set': run_in_hour},
+                'end_time': {'set': 'NOW'},
+                'status': {'set': 'Finished'},
+                'date_modified': {'set': 'NOW'}
+               }
         # print("on_train_end", send)
-        self.submit(send)
+        self.log_messages.append(send)
+        if self.has_solr_config:
+            self.submit(send)
+
+        # save to file when finished
+        self.save()
 
     def submit(self, send):
+        """Send json to solr
+
+        Arguments:
+        send -- json object
+        """
         try:
             requests.post(self.root + self.path,
-                          json=send,
+                          json=[send],
                           headers=self.headers)
         except requests.exceptions.RequestException:
-            warnings.warn('Warning: could not reach RemoteMonitor '
-                          'root server at ' + str(self.root))
+            warnings.warn(
+                'Warning: could not reach RemoteMonitor root server at ' + str(self.root))
+
+    def save(self):
+        """Save log_messages to file
+        """
+        path = os.getenv('TURBINE_OUTPUT') if 'TURBINE_OUTPUT' in os.environ else '.'
+        with open(path + "/run.json", "a") as file_run_json:
+            file_run_json.write(json.dumps(self.log_messages, indent=4, separators=(',', ': ')))
