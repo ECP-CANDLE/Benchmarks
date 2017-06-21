712e8a4910f9d7ab2631b65f4fca7cf1bf4cc49c
Harry Yoo <hsyoo@anl.gov>
2017-06-21 15:52:02 -0500

    add logging

--- Pilot1/NT3/nt3_baseline_keras2.py
+++ Pilot1/NT3/nt3_baseline_keras2.py
@@ -28,6 +28,7 @@ sys.path.append(lib_path2)
 
 import data_utils
 import p1_common
+from solr_keras import CandleRemoteMonitor
 
 #url_nt3 = 'ftp://ftp.mcs.anl.gov/pub/candle/public/benchmarks/Pilot1/normal-tumor/'
 #file_train = 'nt_train2.csv'
@@ -244,13 +245,14 @@ def run(gParameters):
     checkpointer = ModelCheckpoint(filepath=path, verbose=1, save_weights_only=False, save_best_only=True)
     csv_logger = CSVLogger('{}/training.log'.format(output_dir))
     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=10, verbose=1, mode='auto', epsilon=0.0001, cooldown=0, min_lr=0)
+    candleRemoteMonitor = CandleRemoteMonitor(params=gParameters)
 
     history = model.fit(X_train, Y_train,
                     batch_size=gParameters['batch_size'],
                     epochs=gParameters['epochs'],
                     verbose=1,
                     validation_data=(X_test, Y_test),
-                    callbacks = [csv_logger, reduce_lr])
+                    callbacks = [csv_logger, reduce_lr, candleRemoteMonitor])
 
     score = model.evaluate(X_test, Y_test, verbose=0)
 
