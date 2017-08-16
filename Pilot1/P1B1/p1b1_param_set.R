4a8c1ba4c988d3113633fb8b467a66a498dff2f6
Fangfang Xia <fangfang.xia@gmail.com>
2017-08-15 22:39:02 -0500

    update param space

--- Pilot1/P1B1/p1b1_param_set.R
+++ Pilot1/P1B1/p1b1_param_set.R
@@ -1,18 +1,43 @@
 # see https://cran.r-project.org/web/packages/ParamHelpers/ParamHelpers.pdfmakeNum
 # the parameter names should match names of the arguments expected by the benchmark
 
-param.set <- makeParamSet(
-  makeDiscreteParam("batch_size", values = c(32, 64, 128)),
-  makeDiscreteParam("dense", values = c("1000", "1000 1000", "1000 1000 1000")),
-  makeDiscreteParam("molecular_num_hidden", values = c("54 12", "32 16 8"),
-  makeDiscreteParam("activation", values = c("relu", "sigmoid", "tanh")),
-  makeDiscreteParam("optimizer", values = c("adam", "sgd", "rmsprop")),
-  makeNumericParam("learning_rate", lower = 0.00001, upper = 0.1),
-  makeNumericParam("drop", lower = 0, upper = 0.9),
-  makeIntegerParam("latent_dim", lower = 2, upper = 1000),
-  makeIntegerParam("epochs", lower = 100, upper = 100),
+# Current best val_corr: 0.96 for ae, 0.86 for vae
+# We are more interested in vae results
 
-  ## DEBUG PARAMETERS: DON'T USE THESE IN PRODUCTION RUN
-  #  makeIntegerParam("feature_subsample", lower=500, upper=500),
-  ## END DEBUG PARAMS
+param.set <- makeParamSet(
+  # we optimize for ae and vae separately
+  makeDiscreteParam("model", values=c("ae", "vae")),
+
+  # latent_dim impacts ae more than vae
+  makeDiscreteParam("latent_dim", values=c(2, 8, 32, 128, 512)),
+
+  # use a subset of 978 landmark features only to speed up training
+  makeDiscreteParam("use_landmark_genes", values=c(True)),
+
+  # large batch_size only makes sense when warmup_lr is on
+  makeDiscreteParam("batch_size", values=c(32, 64, 128, 256, 512, 1024)),
+
+  # use consecutive 978-neuron layers to facilitate residual connections
+  makeDiscreteParam("dense", values=c("2000 600",
+                                      "978 978",
+				      "978 978 978",
+				      "978 978 978 978",
+				      "978 978 978 978 978",
+				      "978 978 978 978 978 978")),
+
+  makeDiscreteParam("residual", values=c(True, False)),
+
+  makeDiscreteParam("activation", values=c("relu", "sigmoid", "tanh")),
+
+  makeDiscreteParam("optimizer", values=c("adam", "sgd", "rmsprop")),
+
+  makeNumericParam("learning_rate", lower=0.00001, upper=0.1),
+
+  makeDiscreteParam("reduce_lr", values=c(True, False)),
+
+  makeDiscreteParam("warmup_lr", values=c(True, False)),
+
+  makeNumericParam("drop", lower=0, upper=0.9),
+
+  makeIntegerParam("epochs", lower=100, upper=200),
 )
