f68a4f079737002caa9a608b1994ed0435585540
Rajeev Jain <rajeeja@gmail.com>
2017-08-21 14:50:08 -0500

    Fix typo in parameter set

--- Pilot1/P1B1/p1b1_param_set.R
+++ Pilot1/P1B1/p1b1_param_set.R
@@ -12,7 +12,7 @@ param.set <- makeParamSet(
   makeDiscreteParam("latent_dim", values=c(2, 8, 32, 128, 512)),
 
   # use a subset of 978 landmark features only to speed up training
-  makeDiscreteParam("use_landmark_genes", values=c(True)),
+  makeDiscreteParam("use_landmark_genes", values=c("True")),
 
   # large batch_size only makes sense when warmup_lr is on
   makeDiscreteParam("batch_size", values=c(32, 64, 128, 256, 512, 1024)),
@@ -20,12 +20,12 @@ param.set <- makeParamSet(
   # use consecutive 978-neuron layers to facilitate residual connections
   makeDiscreteParam("dense", values=c("2000 600",
                                       "978 978",
-				      "978 978 978",
-				      "978 978 978 978",
-				      "978 978 978 978 978",
-				      "978 978 978 978 978 978")),
+              "978 978 978",
+              "978 978 978 978",
+              "978 978 978 978 978",
+              "978 978 978 978 978 978")),
 
-  makeDiscreteParam("residual", values=c(True, False)),
+  makeDiscreteParam("residual", values=c("True", "False")),
 
   makeDiscreteParam("activation", values=c("relu", "sigmoid", "tanh")),
 
@@ -33,11 +33,11 @@ param.set <- makeParamSet(
 
   makeNumericParam("learning_rate", lower=0.00001, upper=0.1),
 
-  makeDiscreteParam("reduce_lr", values=c(True, False)),
+  makeDiscreteParam("reduce_lr", values=c("True", "False")),
 
-  makeDiscreteParam("warmup_lr", values=c(True, False)),
+  makeDiscreteParam("warmup_lr", values=c("True", "False")),
 
   makeNumericParam("drop", lower=0, upper=0.9),
 
-  makeIntegerParam("epochs", lower=100, upper=200),
+  makeIntegerParam("epochs", lower=100, upper=200)
 )
