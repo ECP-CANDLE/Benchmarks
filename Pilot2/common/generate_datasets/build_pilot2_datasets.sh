f32b875e421363b0566529c0473145f3b67a1526
Brian C. Van Essen <vanessen1@llnl.gov>
2017-11-28 20:38:21 -0800

    Updated generation and preprocessing script to extract each frame and provide a neighborhood list for each molecule.  This will allow unsupervised learning techniques to learn features based on neighborhood data.

--- Pilot2/common/generate_datasets/build_pilot2_datasets.sh
+++ Pilot2/common/generate_datasets/build_pilot2_datasets.sh
@@ -6,12 +6,16 @@ shift
 DSTPATH=$1
 echo "reading from $SRCDIR to $DSTPATH"
 
-dir3k1="${SRCDIR}/RAS/3-component-system/DPPC-DOPC-CHOL/af-restraints-290k/simulations/3k-lipids-290k/run10/*us.*-center-mol.xtc"
-dir3k2="${SRCDIR}/RAS/3-component-system/DPPC-DOPC-CHOL/af-restraints-290k/simulations/3k-lipids-290k/run32/*us.*-center-mol.xtc"
-dir3k3="${SRCDIR}/RAS/3-component-system/DPPC-DOPC-CHOL/af-restraints-290k/simulations/3k-lipids-290k/run43/*us.*-center-mol.xtc"
-dir6k1="${SRCDIR}/RAS/3-component-system/DPPC-DOPC-CHOL/af-restraints-290k/6k-lipids/simulations/6k-lipids-290k/run10/*us.*-center-mol.xtc"
-dir6k2="${SRCDIR}/RAS/3-component-system/DPPC-DOPC-CHOL/af-restraints-290k/6k-lipids/simulations/6k-lipids-290k/run32/*us.*-center-mol.xtc"
-dir6k3="${SRCDIR}/RAS/3-component-system/DPPC-DOPC-CHOL/af-restraints-290k/6k-lipids/simulations/6k-lipids-290k/run43/*us.*-center-mol.xtc"
+dir3k1="RAS/3-component-system/DPPC-DOPC-CHOL/af-restraints-290k/simulations/3k-lipids-290k/run10"
+dir3k2="RAS/3-component-system/DPPC-DOPC-CHOL/af-restraints-290k/simulations/3k-lipids-290k/run32"
+dir3k3="RAS/3-component-system/DPPC-DOPC-CHOL/af-restraints-290k/simulations/3k-lipids-290k/run43"
+dir6k1="RAS/3-component-system/DPPC-DOPC-CHOL/af-restraints-290k/6k-lipids/simulations/6k-lipids-290k/run10"
+dir6k2="RAS/3-component-system/DPPC-DOPC-CHOL/af-restraints-290k/6k-lipids/simulations/6k-lipids-290k/run32"
+dir6k3="RAS/3-component-system/DPPC-DOPC-CHOL/af-restraints-290k/6k-lipids/simulations/6k-lipids-290k/run43"
+ADJ=
+traj_glob="*us.*.tpr"
+
+TMPDIR="/l/ssd"
 
 DATE=`date +%m%d%Y`
 CANDLEDIR="CANDLE_benchmarks_$DATE"
@@ -19,22 +23,25 @@ DSTDIR="${DSTPATH}/${CANDLEDIR}"
 
 mkdir -p "${DSTPATH}"
 
-#for i in $dir3k1 $dir3k2 $dir3k3 $dir6k1 $dir6k2 $dir6k3 
-for i in $dir6k2 $dir6k3 
+for i in $dir3k1 $dir3k2 $dir3k3 $dir6k1 $dir6k2 $dir6k3
 do
     if [ "$i" == $dir3k1 ] ; then hh="3k_run10"; fi
     if [ "$i" == $dir3k2 ] ; then hh="3k_run32"; fi
     if [ "$i" == $dir3k3 ] ; then hh="3k_run43"; fi
     if [ "$i" == $dir6k1 ] ; then hh="6k_run10"; fi
-    if [ "$i" == $dir6k2 ] ; then hh="6k_run32"; fi
-    if [ "$i" == $dir6k3 ] ; then hh="6k_run43"; fi
+    if [ "$i" == $dir6k2 ] ; then hh="6k_run32"; ADJ="skip-first-50ns."; fi
+    if [ "$i" == $dir6k3 ] ; then hh="6k_run43"; ADJ="skip-first-50ns."; fi
+    mkdir -p ${TMPDIR}/${i}
+    glob="*us.*-center-mol.${ADJ}xtc"
+    `cp -r ${SRCDIR}/${i}/${glob} ${TMPDIR}/${i}`
+    `cp -r ${SRCDIR}/${i}/${traj_glob} ${TMPDIR}/${i}`
     echo hh "$hh"
-    hx=`basename $i .xtc`
+    hx=`basename ${TMPDIR}/$i/${glob} .xtc`
     hx=`echo $hx | sed "s/-center-mol//"`
     hy=`echo "$hh"_"$hx"`
-    j=`echo $i | sed "s/.xtc//"`
-    m=`echo $i | sed "s/-center-mol.xtc//"`
-    k=`echo $i | sed 's=../==' | sed 's/.xtc//' | sed "s/\//_/g"| sed 's/\//_/'`
+    j=`echo ${TMPDIR}/$i/${glob} | sed "s/.xtc//"`
+    m=`echo ${TMPDIR}/$i/${glob} | sed "s/-center-mol.${ADJ}xtc//"`
+    k=`echo ${TMPDIR}/$i/${glob} | sed 's=../==' | sed 's/.xtc//' | sed "s/\//_/g"| sed 's/\//_/'`
     dest=${DSTDIR}"/"$hy".dir"
     mkdir -p "$dest"
     chown ${USER}:kras $dest
--- Pilot2/common/pilot2_datasets.py
+++ Pilot2/common/pilot2_datasets.py
@@ -2,7 +2,7 @@ data_sets = {
   '3k_Disordered' : ('3k_run10_10us.35fs-DPPC.10-DOPC.70-CHOL.20.dir', 'ece75b704ec63ac9c39afd74b63497dc'),
   '3k_Ordered' : ('3k_run32_10us.35fs-DPPC.50-DOPC.10-CHOL.40.dir', '211e1bcf46a3f19a978e4af63f067ce0'),
   '3k_Ordered_and_gel' : ('3k_run43_10us.35fs-DPPC.70-DOPC.10-CHOL.20.dir', '87032ff78e4d01739aef5c6c0f5e4f04'),
-  '6k_Disordered' : ('6k_run10_25us.35fs-DPPC.10-DOPC.70-CHOL.20.dir', '24e4f8d3e32569e8bdd2252f7259a65b'),
+  '6k_Disordered' : ('6k_run10_25us.35fs-DPPC.10-DOPC.70-CHOL.20.dir', '13404cb8225819577e4821a976e9203b'),
   '6k_Ordered' : ('6k_run32_25us.35fs-DPPC.50-DOPC.10-CHOL.40.dir', '0b3b39086f720f73ce52d5b07682570d'),
   '6k_Ordered_and_gel' : ('6k_run43_25us.35fs-DPPC.70-DOPC.10-CHOL.20.dir', '3b3e069a7c55a4ddf805f5b898d6b1d1')
   }
