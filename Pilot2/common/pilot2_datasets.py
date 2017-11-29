a3a609e546e1c33ea4a8cbde6f9424c78d504deb
Brian C. Van Essen <vanessen1@llnl.gov>
2017-11-28 18:14:08 -0800

    Updated P2 dataset hash values

--- Pilot2/common/pilot2_datasets.py
+++ Pilot2/common/pilot2_datasets.py
@@ -1,7 +1,7 @@
 data_sets = {
   '3k_Disordered' : ('3k_run10_10us.35fs-DPPC.10-DOPC.70-CHOL.20.dir', 'ece75b704ec63ac9c39afd74b63497dc'),
-  '3k_Ordered' : ('3k_run32_10us.35fs-DPPC.50-DOPC.10-CHOL.40.dir', '6de30893cecbd9c66ea433df0122b328'),
-  '3k_Ordered_and_gel' : ('3k_run43_10us.35fs-DPPC.70-DOPC.10-CHOL.20.dir', '45b9a2f7deefb8d5b016b1c42f5fba71'),
+  '3k_Ordered' : ('3k_run32_10us.35fs-DPPC.50-DOPC.10-CHOL.40.dir', '211e1bcf46a3f19a978e4af63f067ce0'),
+  '3k_Ordered_and_gel' : ('3k_run43_10us.35fs-DPPC.70-DOPC.10-CHOL.20.dir', '87032ff78e4d01739aef5c6c0f5e4f04'),
   '6k_Disordered' : ('6k_run10_25us.35fs-DPPC.10-DOPC.70-CHOL.20.dir', '24e4f8d3e32569e8bdd2252f7259a65b'),
   '6k_Ordered' : ('6k_run32_25us.35fs-DPPC.50-DOPC.10-CHOL.40.dir', '0b3b39086f720f73ce52d5b07682570d'),
   '6k_Ordered_and_gel' : ('6k_run43_25us.35fs-DPPC.70-DOPC.10-CHOL.20.dir', '3b3e069a7c55a4ddf805f5b898d6b1d1')
