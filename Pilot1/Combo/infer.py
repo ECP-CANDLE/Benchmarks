e5b233bf7a4f16780723801a9e81227c74b08051
Fangfang Xia <fangfang.xia@gmail.com>
2017-11-30 00:27:45 -0500

    minor update

--- Pilot1/Combo/infer.py
+++ Pilot1/Combo/infer.py
@@ -3,9 +3,9 @@
 from __future__ import division, print_function
 
 import pandas as pd
-
 import keras
 from keras.models import Model
+from tqdm import tqdm
 
 import NCI60
 
@@ -35,28 +35,36 @@ def prepare_data(sample_set='NCI60', drug_set='ALMANAC'):
 
 
 def main():
-    model = keras.models.load_model('saved_model.h5')
+    model = keras.models.load_model('saved.model.h5')
+    model.load_weights('saved.weights.h5')
     model.summary()
-    df_expr, df_desc = prepare_data(sample_set='NCI60')
-    # df_expr, df_desc = prepare_data(sample_set='NCIPDM')
-    df_sample_ids = df_expr[['Sample']].head(10)
-    df_drug_ids = df_desc[['Drug']].head()
-    # df_sample_ids = df_expr[['Sample']].copy()
-    # df_drug_ids = df_desc[['Drug']].copy()
+    # df_expr, df_desc = prepare_data(sample_set='NCI60')
+    df_expr, df_desc = prepare_data(sample_set='NCIPDM')
+    # df_sample_ids = df_expr[['Sample']].head(10)
+    # df_drug_ids = df_desc[['Drug']].head()
+    df_sample_ids = df_expr[['Sample']].copy()
+    df_drug_ids = df_desc[['Drug']].copy()
     df_all = cross_join3(df_sample_ids, df_drug_ids, df_drug_ids, suffixes=('1', '2'))
 
-    x_all_list = []
-    df_x_all = pd.merge(df_all[['Sample']], df_expr, on='Sample', how='left')
-    x_all_list.append(df_x_all.drop(['Sample'], axis=1).values)
+    step = 1024
+    total = df_all.shape[0]
+    for i in tqdm(range(0, total, step)):
+        j = min(i+step, total)
 
-    drugs = ['Drug1', 'Drug2']
-    for drug in drugs:
-        df_x_all = pd.merge(df_all[[drug]], df_desc, left_on=drug, right_on='Drug', how='left')
-        x_all_list.append(df_x_all.drop([drug, 'Drug'], axis=1).values)
+        x_all_list = []
+        df_x_all = pd.merge(df_all[['Sample']].iloc[i::j], df_expr, on='Sample', how='left')
+        x_all_list.append(df_x_all.drop(['Sample'], axis=1).values)
 
-    y_pred = model.predict(x_all_list, batch_size=128).flatten()
+        drugs = ['Drug1', 'Drug2']
+        for drug in drugs:
+            df_x_all = pd.merge(df_all[[drug]].iloc[i::j], df_desc, left_on=drug, right_on='Drug', how='left')
+            x_all_list.append(df_x_all.drop([drug, 'Drug'], axis=1).values)
 
-    df_all['Pred_Growth'] = y_pred
+        y_pred = model.predict(x_all_list, batch_size=512, verbose=0).flatten()
+        df_all.loc[i::j, 'Pred_Growth'] = y_pred
+
+    # df_all['Pred_Growth'] = y_pred
+    df_all.to_csv('tmp.csv', index=False, float_format='%.4f')
 
 
 if __name__ == '__main__':
