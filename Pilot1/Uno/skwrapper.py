64ee43691ce1ae477520ebdc0b7697489de7b447
Fangfang Xia <fangfang.xia@gmail.com>
2018-05-16 15:47:25 -0500

    disable lgbm warning

--- Pilot1/Uno/skwrapper.py
+++ Pilot1/Uno/skwrapper.py
@@ -29,7 +29,7 @@ warnings.filterwarnings(action="ignore", category=DeprecationWarning)
 def get_model(model_or_name, threads=-1, classify=False, seed=0):
     regression_models = {
         'xgboost': (XGBRegressor(max_depth=6, n_jobs=threads, random_state=seed), 'XGBRegressor'),
-        'lightgbm': (LGBMRegressor(n_jobs=threads, random_state=seed), 'LGBMRegressor'),
+        'lightgbm': (LGBMRegressor(n_jobs=threads, random_state=seed, verbose=-1), 'LGBMRegressor'),
         'randomforest': (RandomForestRegressor(n_estimators=100, n_jobs=threads), 'RandomForestRegressor'),
         'adaboost': (AdaBoostRegressor(), 'AdaBoostRegressor'),
         'linear': (LinearRegression(), 'LinearRegression'),
@@ -39,15 +39,15 @@ def get_model(model_or_name, threads=-1, classify=False, seed=0):
 
         'xgb.1k': (XGBRegressor(max_depth=6, n_estimators=1000, n_jobs=threads, random_state=seed), 'XGBRegressor.1K'),
         'xgb.10k': (XGBRegressor(max_depth=6, n_estimators=10000, n_jobs=threads, random_state=seed), 'XGBRegressor.10K'),
-        'lgbm.1k': (LGBMRegressor(n_estimators=1000, n_jobs=threads, random_state=seed), 'LGBMRegressor.1K'),
-        'lgbm.10k': (LGBMRegressor(n_estimators=10000, n_jobs=threads, random_state=seed), 'LGBMRegressor.10K'),
+        'lgbm.1k': (LGBMRegressor(n_estimators=1000, n_jobs=threads, random_state=seed, verbose=-1), 'LGBMRegressor.1K'),
+        'lgbm.10k': (LGBMRegressor(n_estimators=10000, n_jobs=threads, random_state=seed, verbose=-1), 'LGBMRegressor.10K'),
         'rf.1k': (RandomForestRegressor(n_estimators=1000, n_jobs=threads), 'RandomForestRegressor.1K'),
         'rf.10k': (RandomForestRegressor(n_estimators=10000, n_jobs=threads), 'RandomForestRegressor.10K')
     }
 
     classification_models = {
         'xgboost': (XGBClassifier(max_depth=6, n_jobs=threads, random_state=seed), 'XGBClassifier'),
-        'lightgbm': (LGBMClassifier(n_jobs=threads, random_state=seed), 'LGBMClassifier'),
+        'lightgbm': (LGBMClassifier(n_jobs=threads, random_state=seed, verbose=-1), 'LGBMClassifier'),
         'randomforest': (RandomForestClassifier(n_estimators=100, n_jobs=threads), 'RandomForestClassifier'),
         'adaboost': (AdaBoostClassifier(), 'AdaBoostClassifier'),
         'logistic': (LogisticRegression(), 'LogisticRegression'),
@@ -58,8 +58,8 @@ def get_model(model_or_name, threads=-1, classify=False, seed=0):
 
         'xgb.1k': (XGBClassifier(max_depth=6, n_estimators=1000, n_jobs=threads, random_state=seed), 'XGBClassifier.1K'),
         'xgb.10k': (XGBClassifier(max_depth=6, n_estimators=10000, n_jobs=threads, random_state=seed), 'XGBClassifier.10K'),
-        'lgbm.1k': (LGBMClassifier(n_estimators=1000, n_jobs=threads, random_state=seed), 'LGBMClassifier.1K'),
-        'lgbm.10k': (LGBMClassifier(n_estimators=1000, n_jobs=threads, random_state=seed), 'LGBMClassifier.10K'),
+        'lgbm.1k': (LGBMClassifier(n_estimators=1000, n_jobs=threads, random_state=seed, verbose=-1), 'LGBMClassifier.1K'),
+        'lgbm.10k': (LGBMClassifier(n_estimators=1000, n_jobs=threads, random_state=seed, verbose=-1), 'LGBMClassifier.10K'),
         'rf.1k': (RandomForestClassifier(n_estimators=1000, n_jobs=threads), 'RandomForestClassifier.1K'),
         'rf.10k': (RandomForestClassifier(n_estimators=10000, n_jobs=threads), 'RandomForestClassifier.10K')
     }
