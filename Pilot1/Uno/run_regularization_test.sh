aae02226c49e9895ca5fcbec290357bd49f6c6df
aclyde11 <aclyde11@gmail.com>
2019-01-15 18:37:02 -0600

    added git pull

--- Pilot1/Uno/run_regularization_test.sh
+++ Pilot1/Uno/run_regularization_test.sh
@@ -4,4 +4,4 @@ if [ "$1" != "-p" ]; then
 fi
 rm -r cache
 mkdir cache
-python uno_baseline_keras2.py --train_sources CCLE --cache cache/CCLE --use_landmark_genes False --preprocess_rnaseq source_scale --no_feature_source True --no_response_source True --gpus 0 1 --epochs 50 --genemania_regularizers True --genemania_filepath ../../../GraphCovTestPMDR/GeneMania_adj.hdf
+python uno_baseline_keras2.py --train_sources CCLE --cache cache/CCLE --use_landmark_genes False --preprocess_rnaseq source_scale --no_feature_source True --no_respnse_source True --gpus 0 1 --epochs 50 --genemania_regularizers True --genemania_filepath ../../../GraphCovTestPMDR/GeneMania_adj.hdf
