4ae6eeae63b947f3c3c9fbeb7035c92b9c68a04b
Austin Clyde <aclyde@anl.gov>
2019-01-15 19:10:31 -0600

    added req file

--- Pilot1/Uno/run_regularization_test.sh
+++ Pilot1/Uno/run_regularization_test.sh
@@ -2,6 +2,9 @@
 if [ "$0" != "-p" ]; then
     git pull origin master
 fi
+
+source /raid/aclyde11/gene-graph-conv/graphconv_env/bin/activate
+export CUDA_VISABLE_DEVICES='0,1'
 rm -r cache
 mkdir cache
 python uno_baseline_keras2.py --train_sources CCLE --cache cache/CCLE --use_landmark_genes False --preprocess_rnaseq source_scale --no_feature_source True --no_response_source True --gpus 0 1 --epochs 50 --genemania_regularizers True --genemania_filepath ../../../GraphCovTestPMDR/GeneMania_adj.hdf
