51153e2d1465840f953f3302ce07b34c72e1870d
aclyde11 <aclyde11@gmail.com>
2019-01-19 10:45:15 -0600

    added git pull

--- Pilot1/RNAseqToSnp/RNAseqToSnp.py
+++ Pilot1/RNAseqToSnp/RNAseqToSnp.py
@@ -46,6 +46,7 @@ def get_activations(model, inputs, print_shape_only=False, layer_name=None):
         outputs = [layer.output for layer in model.layers]
     else:
         outputs = [layer.output for layer in model.layers if layer.name == layer_name]  # all layer outputs
+    print outputs
     funcs = [K.function([inp] + [K.learning_phase()], [out]) for out in outputs]  # evaluation functions
     layer_outputs = [func([inputs, 1.])[0] for func in funcs]
     for layer_activations in layer_outputs:
