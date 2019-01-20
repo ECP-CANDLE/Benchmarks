eee60de6aab65791bde0c6fc3d3db1a825c96569
aclyde11 <aclyde11@gmail.com>
2019-01-20 07:40:11 -0600

    added git pull

--- Pilot1/RNAseqToSnp/RNAseqToSnp.py
+++ Pilot1/RNAseqToSnp/RNAseqToSnp.py
@@ -13,7 +13,7 @@ from sklearn import preprocessing, utils, ensemble, feature_selection, model_sel
 import talos as ta
 
 from RNAseqParse import DataLoader
-
+from RnaToSnpGridSearch import snps_from_rnaseq_grid_search
 
 def arg_setup():
     parser = argparse.ArgumentParser()
@@ -102,12 +102,6 @@ def build_feature_model_genes(input_shape, name='', dense_layers=[500, 500],
     return model
 
 
-def r2(y_true, y_pred):
-    SS_res = K.sum(K.square(y_true - y_pred))
-    SS_tot = K.sum(K.square(y_true - K.mean(y_true)))
-    return (1 - SS_res / (SS_tot + K.epsilon()))
-
-
 def build_model(input_dim, output_shape):
     x_input = Input(shape=(input_dim,))
     attention_probs = Dense(input_dim, activation='softmax', name='attention_vec')(x_input)
@@ -140,49 +134,6 @@ def build_autoencoder(input_dim, encoded_dim=1000, output_dim=1):
     print model_snp.summary()
     return model_autoencoder, model_snp
 
-
-def breast_cancer_model(x_train, y_train, x_val, y_val, params):
-    x_input = Input(shape=(x_train.shape[0],))
-    x = Dense(params['first_neuron'], activation=params['activation'], kernel_initializer=params['kernel_initializer'])(
-        x_input)
-    layer = Dropout(params['dropout'])(x)
-    encoded = Dense(params['encoded_dim'], activation=params['activation'],
-                    kernel_initializer=params['kernel_initializer'])(x)
-
-    h = None
-    if params['use_attention']:
-        attention_probs = Dense(params['encoded_dim'], activation='softmax', name='attention_vec')(encoded)
-        attention_mul = multiply([encoded, attention_probs], name='attention_mul')
-        h = x
-    else:
-        h = encoded
-    x = Dense(params['encoded_dim'], activation=params['activation'], kernel_initializer=params['kernel_initializer'])(
-        h)
-    x = Dense(params['hidden_unit'])(x)
-    snp_guess = Dense(y_train.shape[1], activation=params['snp_activation'])(x)
-
-    x = Dense(params['first_neuron'], activation=params['activation'], kernel_initializer=params['kernel_initializer'])(
-        encoded)
-    decoded = Dense(x_train.shape[1], activation=params['last_activation'],
-                    kernel_initializer=params['kernel_initializer'])(x)
-
-    model_snps = Model(inputs=x_input, outputs=snp_guess)
-    model_snps = multi_gpu_model(model_snps, 2)
-    model_auto = Model(inputs=x_input, outputs=decoded)
-    model_auto = multi_gpu_model(model_auto, 2)
-    model_auto.compile(loss=params['auto_losses'], optimizer=optimizers.adam(), metrics=['acc'])
-    model_snps.compile(loss=params['losses'],
-                       optimizer=params['optimizer'], metrics=['accuracy', r2, 'mae', 'mse'])
-
-    model_auto.fit(x_train, x_train, epochs=20, batch_size=200, verbose=0)
-
-    history = model_snps.fit(x_train, y_train,
-                             validation_data=[x_val, y_val],
-                             batch_size=params['batch_size'],
-                             epochs=params['epochs'],
-                             verbose=0)
-
-    return history, model_snps
 
 
 def create_class_weight(labels_dict, y):
@@ -335,79 +286,6 @@ def load_and_normalize_rnaseq(args):
 
 def rna_seq_autoencoder_gridsearch(args):
     rnaseq = load_and_normalize_rnaseq(args)
-
-def snps_from_rnaseq_grid_search(args):
-    loader = DataLoader(args.data_path, args)
-    snps, rnaseq = loader.load_aligned_snps_rnaseq(use_reduced=True, align_by=args.reduce_snps)
-    rnaseq = rnaseq.set_index("Sample")
-    cols = rnaseq.columns.to_series()
-    index = rnaseq.index.to_series()
-    rnaseq = pd.DataFrame(preprocessing.scale(rnaseq), columns=cols, index=index)
-    # intersect = set(snps.columns.to_series()).intersection(set((loader.load_oncogenes_()['oncogenes'])))
-    # filter_snps_oncogenes = snps[list(intersect)]
-    x_big = rnaseq
-
-    samples = set(rnaseq.index.to_series()).intersection(set(snps.index.to_series()))
-    y = snps.loc[samples]
-    x = rnaseq.loc[samples]
-    y = y.sort_index(axis=0)
-    x = x.sort_index(axis=0)
-
-    y = y[['ENSG00000181143', 'ENSG00000145113', 'ENSG00000127914', 'ENSG00000149311']]
-
-    print y.tail()
-    print x.tail()
-    print x.shape, y.shape
-
-    print y.describe()
-    y = np.array(y, dtype=np.float32)
-    if args.y_scale == 'max1':
-        y = np.minimum(y, np.ones(y.shape))
-    elif args.y_scale == 'scale':
-        print "roubust scaling"
-        scaler = preprocessing.MinMaxScaler()
-        shape = y.shape
-        y = scaler.fit_transform(y)
-    print "Procressed y:"
-
-    # then we can go ahead and set the parameter space
-    # p = {'first_neuron': (50, 2000, 200),
-    #      'batch_size': (1, 200, 20),
-    #      'epochs': (10, 200, 20),
-    #      'dropout': (0, 0.3, 0.1),
-    #      'kernel_initializer': ['uniform', 'normal'],
-    #      'use_attention': [True, False],
-    #      'encoded_dim': (10, 1000, 100),
-    #      'auto_losses': [keras.losses.mse, keras.losses.kullback_leibler_divergence, keras.losses.mae],
-    #      'optimizer': [keras.optimizers.nadam, keras.optimizers.adam, keras.optimizers.SGD],
-    #      'losses': [keras.losses.categorical_crossentropy, keras.losses.categorical_hinge,
-    #                 keras.losses.sparse_categorical_crossentropy],
-    #      'activation': [keras.activations.relu, keras.activations.elu, keras.activations.sigmoid],
-    #      'last_activation': [keras.activations.sigmoid, keras.activations.relu]}
-    p = {'first_neuron': [200],
-         'hidden_unit': [200],
-         'batch_size': [200],
-         'epochs': [5],
-         'dropout': [0],
-         'kernel_initializer': ['uniform', 'normal'],
-         'use_attention': [True, False],
-         'encoded_dim': [100],
-         'auto_losses': ['mse', 'mae'],
-         'optimizer': ['sgd'],
-         'losses': ['keras.losses.categorical_crossentropy'],
-         'activation': ['relu', 'elu', 'sigmoid'],
-         'last_activation': ['sigmoid'],
-         'snp_activation': ['sigmoid']}
-    x = np.array(x, dtype=np.float32)
-    y = np.array(x, dtype=np.float32)
-    t = ta.Scan(x, y,
-                params=p,
-                model=breast_cancer_model,
-                grid_downsample=1,
-                random_method='quantum',
-                dataset_name="RNA Autoencoder pretained snp 'ENSG00000181143', 'ENSG00000145113', 'ENSG00000127914', 'ENSG00000149311'",
-                experiment_no='1', val_split=0.2, debug=True, print_params=True)
-    r = ta.Reporting("breast_cancer_1.csv")
 
 
 def main_snp_autoencoder(args):
