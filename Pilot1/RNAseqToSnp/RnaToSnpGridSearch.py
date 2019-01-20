0719b85676f947b8db85c18593c964092e171250
aclyde11 <aclyde11@gmail.com>
2019-01-20 10:55:49 -0600

    added git pull

--- Pilot1/RNAseqToSnp/RnaToSnpGridSearch.py
+++ Pilot1/RNAseqToSnp/RnaToSnpGridSearch.py
@@ -123,7 +123,8 @@ def snp_snp_gridsearch_model(x_train, y_train, x_val, y_val, params):
 
     model_auto = Model(inputs=x_input, outputs=decoded)
     print(gpu_nums)
-    model_auto = multi_gpu_model(model_auto, gpus=gpu_nums)
+    if gpu_nums > 1:
+        model_auto = multi_gpu_model(model_auto, gpus=gpu_nums)
     model_auto.compile(loss=params['auto_losses'], optimizer=params['optimizer'](lr=params['lr']), metrics=['acc', r2])
 
     history = model_auto.fit(x_train, y_train, validation_data=[x_val, y_val], epochs=params['epochs'],
