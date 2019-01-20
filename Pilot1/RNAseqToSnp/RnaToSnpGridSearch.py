de5d61c6729ec632a919ec9eeba61c979a87d1de
aclyde11 <aclyde11@gmail.com>
2019-01-20 08:43:55 -0600

    added git pull

--- Pilot1/RNAseqToSnp/RnaToSnpGridSearch.py
+++ Pilot1/RNAseqToSnp/RnaToSnpGridSearch.py
@@ -16,6 +16,69 @@ from RNAseqParse import DataLoader
 from metrics import r2
 
 
+
+
+
+def rna_rna_gridsearch_model(x_train, y_train, x_val, y_val, params):
+    x_input = Input(shape=(x_train.shape[1],))
+    x = Dense(params['first_neuron'], activation=params['activation'], kernel_initializer=params['kernel_initializer'])(
+        x_input)
+    layer = Dropout(params['dropout'])(x)
+    encoded = Dense(params['encoded_dim'], activation=params['activation'],
+                    kernel_initializer=params['kernel_initializer'])(x)
+
+    x = Dense(params['first_neuron'], activation=params['activation'], kernel_initializer=params['kernel_initializer'])(
+        encoded)
+    decoded = Dense(x_train.shape[1], activation=params['last_activation'],
+                    kernel_initializer=params['kernel_initializer'])(x)
+
+    model_auto = Model(inputs=x_input, outputs=decoded)
+    model_auto = multi_gpu_model(model_auto, 2)
+    model_auto.compile(loss=params['auto_losses'], optimizer=params['optimizer'](lr=params['lr']), metrics=['acc', r2])
+
+    history = model_auto.fit(x_train, y_train, validation_data=[x_val, y_val], epochs=params['epochs'],
+                             batch_size=params['batch_size'], verbose=0)
+
+    return history, model_auto
+
+
+def rna_rna_gridsearch_params():
+    params = {'first_neuron': (100, 2000, 5),
+              'batch_size': (1, 200, 5),
+              'epochs': (10, 75, 4),
+              'dropout': (0, 0.3, 3),
+              'kernel_initializer': ['uniform', 'normal'],
+              'encoded_dim': (10, 2000, 10),
+              'auto_losses': ['mse', 'kullback_leibler_divergence', 'mae'],
+              'optimizer': [keras.optimizers.nadam, keras.optimizers.adam, keras.optimizers.SGD],
+              'lr': (0.001, 2, 10),
+              'activation': ['sigmoid', 'elu', 'relu'],
+              'last_activation': ['sigmoid', 'elu']}
+    return params
+
+
+def rna_rna_gridsearch(args):
+    loader = DataLoader(args.data_path, args)
+    _, rnaseq = loader.load_aligned_snps_rnaseq(use_reduced=True, align_by=args.reduce_snps)
+    rnaseq = rnaseq.set_index("Sample")
+    x = preprocessing.scale(rnaseq)
+
+    x = np.array(x, dtype=np.float32)
+    y = np.array(x, dtype=np.float32)
+
+    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2)
+
+    t = ta.Scan(x_train, y_train, x_val=x_val, y_val=y_val,
+                params=rna_rna_gridsearch_params(),
+                model=rna_rna_gridsearch_model,
+                grid_downsample=0.01,
+                reduction_metric='val_r2',
+                reduction_method='correlation',
+                dataset_name="RNA Autoencoder on all RNAseq",
+                experiment_no='1', debug=True, print_params=True)
+    r = ta.Reporting("rna_autoencoder.csv")
+
+
 def rna_snp_pt_gridsearch(x_train, y_train, x_val, y_val, params):
     x_input = Input(shape=(x_train.shape[1],))
     x = Dense(params['first_neuron'], activation=params['activation'], kernel_initializer=params['kernel_initializer'])(
@@ -60,65 +123,21 @@ def rna_snp_pt_gridsearch(x_train, y_train, x_val, y_val, params):
     return history, model_snps
 
 
-def rna_rna_gridsearch_model(x_train, y_train, x_val, y_val, params):
-    x_input = Input(shape=(x_train.shape[1],))
-    x = Dense(params['first_neuron'], activation=params['activation'], kernel_initializer=params['kernel_initializer'])(
-        x_input)
-    layer = Dropout(params['dropout'])(x)
-    encoded = Dense(params['encoded_dim'], activation=params['activation'],
-                    kernel_initializer=params['kernel_initializer'])(x)
-
-    x = Dense(params['first_neuron'], activation=params['activation'], kernel_initializer=params['kernel_initializer'])(
-        encoded)
-    decoded = Dense(x_train.shape[1], activation=params['last_activation'],
-                    kernel_initializer=params['kernel_initializer'])(x)
-
-    model_auto = Model(inputs=x_input, outputs=decoded)
-    model_auto = multi_gpu_model(model_auto, 2)
-    model_auto.compile(loss=params['auto_losses'], optimizer=params['optimizer'](lr=params['lr']), metrics=['acc', r2])
-
-    history = model_auto.fit(x_train, y_train, validation_data=[x_val, y_val], epochs=params['epochs'],
-                             batch_size=params['batch_size'], verbose=0)
-
-    return history, model_auto
-
-
-def rna_rna_gridsearch_params():
-    params = {'first_neuron': (10, 2000, 10),
-              'batch_size': (1, 200, 10),
-              'epochs': (10, 200, 5),
-              'dropout': (0, 0.3, 2),
-              'kernel_initializer': ['uniform', 'normal'],
-              'encoded_dim': (10, 2000, 10),
-              'auto_losses': ['mse', 'kullback_leibler_divergence', 'mae'],
-              'optimizer': [keras.optimizers.nadam, keras.optimizers.adam, keras.optimizers.SGD],
-              'lr': (0.001, 2, 10),
-              'activation': ['sigmoid', 'elu', 'relu'],
-              'last_activation': ['sigmoid', 'elu']}
-    return params
-
-
-def rna_rna_gridsearch(args):
-    loader = DataLoader(args.data_path, args)
-    _, rnaseq = loader.load_aligned_snps_rnaseq(use_reduced=True, align_by=args.reduce_snps)
-    rnaseq = rnaseq.set_index("Sample")
-    x = preprocessing.scale(rnaseq)
-
-    x = np.array(x, dtype=np.float32)
-    y = np.array(x, dtype=np.float32)
-
-    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2)
-
-    t = ta.Scan(x_train, y_train, x_val=x_val, y_val=y_val,
-                params=rna_rna_gridsearch_params(),
-                model=rna_rna_gridsearch_model,
-                grid_downsample=0.01,
-                reduction_metric='val_r2',
-                dataset_name="RNA Autoencoder on all RNAseq",
-                experiment_no='1', debug=True, print_params=True)
-    r = ta.Reporting("rna_autoencoder.csv")
-
-
+def snps_from_rnaseq_params():
+    p = {'first_neuron': (10, 2000, 10),
+         'batch_size': (1, 200, 10),
+         'epochs': (10, 200, 5),
+         'dropout': (0, 0.3, 2),
+         'hidden_unit': (100, 1000, 10),
+         'kernel_initializer': ['uniform', 'normal'],
+         'use_attention': [True, False],
+         'encoded_dim': (10, 1000, 10),
+         'auto_losses': ['mse', 'kullback_leibler_divergence', 'mae'],
+         'optimizer': ['nadam', 'adam', 'SGD'],
+         'losses': ['categorical_crossentropy', 'categorical_hinge', 'sparse_categorical_crossentropy'],
+         'activation': ['sigmoid', 'elu', 'elu'],
+         'last_activation': ['sigmoid', 'elu']}
+    return p
 
 def snps_from_rnaseq_grid_search(args):
     loader = DataLoader(args.data_path, args)
@@ -155,24 +174,15 @@ def snps_from_rnaseq_grid_search(args):
     print "Procressed y:"
 
     # then we can go ahead and set the parameter space
-    p = {'first_neuron': (10, 2000, 10),
-         'batch_size': (1, 200, 10),
-         'epochs': (10, 200, 5),
-         'dropout': (0, 0.3, 2),
-         'hidden_unit': (100, 1000, 10),
-         'kernel_initializer': ['uniform', 'normal'],
-         'use_attention': [True, False],
-         'encoded_dim': (10, 1000, 10),
-         'auto_losses': ['mse', 'kullback_leibler_divergence', 'mae'],
-         'optimizer': ['nadam', 'adam', 'SGD'],
-         'losses': ['categorical_crossentropy', 'categorical_hinge', 'sparse_categorical_crossentropy'],
-         'activation': ['sigmoid', 'elu', 'elu'],
-         'last_activation': ['sigmoid', 'elu']}
+
 
     x = np.array(x, dtype=np.float32)
     y = np.array(x, dtype=np.float32)
-    t = ta.Scan(x, y,
-                params=p,
+
+    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.15)
+
+    t = ta.Scan(x_train, y_train, x_val=x_val, y_val=y_val,
+                params=snps_from_rnaseq_params(),
                 model=rna_snp_pt_gridsearch,
                 grid_downsample=0.01,
                 reduction_metric='val_r2',
