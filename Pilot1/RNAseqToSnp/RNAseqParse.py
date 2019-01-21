dc1ac3d44c6c11cfeeefa9bef95949cbbf4518f2
austin <aclyde11@gmail.com>
2019-01-21 10:20:17 -0600

    added query for name

--- Pilot1/RNAseqToSnp/RNAseqParse.py
+++ Pilot1/RNAseqToSnp/RNAseqParse.py
@@ -138,11 +138,11 @@ class DataLoader:
         logging.info("Loading aligned snp rna seq.")
         if len(align_by) == 0:
             align_by = ['None']
-        if os.path.exists(self.cache_path+ "_".join(align_by) + "_" + cached_file[0]):
+        if os.path.exists(self.cache_path + "_".join(align_by) + "_" + cached_file[0]):
             snps =  self.load_hdf("_".join(align_by) + "_" + cached_file[0], key=cached_file[1])
             rnaseq = self.load_hdf("_".join(align_by) + "_" + cached_file[0], key=cached_file[2])
             if "genemania" in align_by:
-                adj = pd.read_hdf("_".join(align_by) + "_" + cached_file[0], key="adj")
+                adj = pd.read_hdf(self.cache_path + "_".join(align_by) + "_" + cached_file[0], key="adj")
                 self.adj = adj
             return snps, rnaseq
 
