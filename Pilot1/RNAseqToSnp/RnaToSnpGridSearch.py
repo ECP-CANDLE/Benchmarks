4dcb8c6e3920de2b6e5bd82916259ab996903a20
aclyde11 <aclyde11@gmail.com>
2019-01-20 18:41:04 -0600

    added git pull

--- Pilot1/RNAseqToSnp/RnaToSnpGridSearch.py
+++ Pilot1/RNAseqToSnp/RnaToSnpGridSearch.py
@@ -223,7 +223,7 @@ def rna_snp_pt_gridsearch(x_train, y_train, x_val, y_val, params):
 
     model_auto.compile(loss=params['auto_losses'], optimizer=params['auto_optimizer'](params['lr'] * 4),
                        metrics=['acc', r2])
-    model_auto.fit(x_big, x_big, epochs=20, batch_size=200, verbose=1, validation_split=0.05)
+    model_auto.fit(x_big, x_big, epochs=2, batch_size=200, verbose=1, validation_split=0.05)
 
     model_snps.compile(loss=params['snp_losses'],
                        optimizer=params['snp_optimizer']('lr'), metrics=['accuracy', r2, 'mae', 'mse'])
