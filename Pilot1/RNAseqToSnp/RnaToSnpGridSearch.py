7b6dfda866e2d0d7c5f0929cece93d856a2165f0
austin <aclyde11@gmail.com>
2019-01-21 09:00:18 -0600

    added query for name

--- Pilot1/RNAseqToSnp/RnaToSnpGridSearch.py
+++ Pilot1/RNAseqToSnp/RnaToSnpGridSearch.py
@@ -62,7 +62,8 @@ def rna_rna_gridsearch_model(x_train, y_train, x_val, y_val, params):
 
     model_auto = Model(inputs=x_input, outputs=decoded)
     print(gpu_nums)
-    model_auto = multi_gpu_model(model_auto, gpus=gpu_nums)
+    if gpu_nums > 1:
+        model_auto = multi_gpu_model(model_auto, gpus=gpu_nums)
     model_auto.compile(loss=params['auto_losses'], optimizer=keras.optimizers.adam(lr=params['lr']), metrics=['acc', r2])
 
     history = model_auto.fit(x_train, y_train, validation_data=[x_val, y_val], epochs=params['epochs'],
