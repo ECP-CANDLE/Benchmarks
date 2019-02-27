944ed4d2a681925be3a236184d04b83aa958365b
Cristina Garcia Cardona <cgarciac@lanl.gov>
2019-02-27 16:10:02 -0700

    removed keras dependence from data_utils

--- common/data_utils.py
+++ common/data_utils.py
@@ -6,10 +6,36 @@ import pandas as pd
 from sklearn.preprocessing import Imputer
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, MaxAbsScaler
 
-from keras.utils import np_utils
-
 from default_utils import DEFAULT_SEED
 from default_utils import DEFAULT_DATATYPE
+
+
+# TAKEN from tensorflow
+def to_categorical(y, num_classes=None):
+  """Converts a class vector (integers) to binary class matrix.
+  E.g. for use with categorical_crossentropy.
+  Arguments:
+      y: class vector to be converted into a matrix
+          (integers from 0 to num_classes).
+      num_classes: total number of classes.
+  Returns:
+      A binary matrix representation of the input. The classes axis is placed
+      last.
+  """
+  y = np.array(y, dtype='int')
+  input_shape = y.shape
+  if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
+    input_shape = tuple(input_shape[:-1])
+  y = y.ravel()
+  if not num_classes:
+    num_classes = np.max(y) + 1
+  n = y.shape[0]
+  categorical = np.zeros((n, num_classes), dtype=np.float32)
+  categorical[np.arange(n), y] = 1
+  output_shape = input_shape + (num_classes,)
+  categorical = np.reshape(categorical, output_shape)
+  return categorical
+
 
 def convert_to_class(y_one_hot, dtype=int):
     """ Converts a one-hot class encoding (array with as many positions as total
@@ -655,8 +681,8 @@ def load_Xy_data_noheader(train_file, test_file, classes, usecols=None, scaling=
     df_y_train = df_train[:,0].astype('int')
     df_y_test = df_test[:,0].astype('int')
 
-    Y_train = np_utils.to_categorical(df_y_train, classes)
-    Y_test = np_utils.to_categorical(df_y_test, classes)
+    Y_train = to_categorical(df_y_train, classes)
+    Y_test = to_categorical(df_y_test, classes)
 
     df_x_train = df_train[:, 1:seqlen].astype(dtype)
     df_x_test = df_test[:, 1:seqlen].astype(dtype)
