5e36428c266f1562559a2d414d847bb007d6fb0e
Hyunseung Yoo <hsyoo@anl.gov>
2019-04-08 15:04:50 -0500

    candle_keras is accessible after setting sys path

--- Pilot1/Uno/uno.py
+++ Pilot1/Uno/uno.py
@@ -9,13 +9,12 @@ try:
 except ImportError:
     import ConfigParser as configparser
 
-import candle_keras as candle
-
 file_path = os.path.dirname(os.path.realpath(__file__))
 lib_path = os.path.abspath(os.path.join(file_path, '..'))
 sys.path.append(lib_path)
 lib_path2 = os.path.abspath(os.path.join(file_path, '..', '..', 'common'))
 sys.path.append(lib_path2)
+import candle_keras as candle
 
 logger = logging.getLogger(__name__)
 
