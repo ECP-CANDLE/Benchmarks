9190311c7a94cc6d3f2d8b57d6b484e54b27e4b2
Andrew Weisman <andrew.weisman@nih.gov>
2019-05-06 15:54:06 -0400

    Allowed uno benchmark to use a default params file variable within the model

--- Pilot1/Uno/uno_baseline_keras2.py
+++ Pilot1/Uno/uno_baseline_keras2.py
@@ -279,7 +279,8 @@ def build_model(loader, args, permanent_dropout=True, silent=False):
 def initialize_parameters():
 
     # Build benchmark object
-    unoBmk = benchmark.BenchmarkUno(benchmark.file_path, 'uno_default_model.txt', 'keras',
+    #mymodel_common = candle.Benchmark(file_path,os.getenv("DEFAULT_PARAMS_FILE"),'keras',prog='myprog',desc='My model')
+    unoBmk = benchmark.BenchmarkUno(benchmark.file_path, os.getenv("DEFAULT_PARAMS_FILE"), 'keras',
     prog='uno_baseline', desc='Build neural network based models to predict tumor response to single and paired drugs.')
     
     # Initialize parameters
