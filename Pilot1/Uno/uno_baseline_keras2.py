c62b561249f8695833f11c9d3073dc6ed5d22e64
brettin <brettin@anl.gov>
2019-09-10 14:59:34 -0400

    added mixed precision support

--- Pilot1/Uno/uno_baseline_keras2.py
+++ Pilot1/Uno/uno_baseline_keras2.py
@@ -414,17 +414,14 @@ def run(params):
         else:
             model = template_model
 
-        optimizer = optimizers.deserialize({'class_name': args.optimizer, 'config': {}})
+        optimizer = tf.keras.optimizers.deserialize({'class_name': args.optimizer, 'config': {}})
         base_lr = args.base_lr or K.get_value(optimizer.lr)
         if args.learning_rate:
             K.set_value(optimizer.lr, args.learning_rate)
 
         if args.mixed_precision:
             print("using mixed precision mode")
-            if args.learning_rate:
-                optimizer = tf.train.AdamOptimizer(learning_rate=args.learning_rate)
-            else:
-                optimizer = tf.train.experimental.enable_mixed_precision_graph_rewrite(optimizer)
+            optimizer = tf.train.experimental.enable_mixed_precision_graph_rewrite(optimizer)
 
         model.compile(loss=args.loss, optimizer=optimizer, metrics=[mae, r2])
 
