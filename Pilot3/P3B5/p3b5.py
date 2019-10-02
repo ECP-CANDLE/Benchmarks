a36b75a80d8f3579d1b3dd1c03812a524d46b7d8
Young, Todd <youngmt1@ornl.gov>
2019-10-02 11:17:14 -0400

    Set hyperparameters

--- Pilot3/P3B5/p3b5.py
+++ Pilot3/P3B5/p3b5.py
@@ -1,23 +1,32 @@
 import candle
-import p3b5 as bmk
 
 
-required = [
-    'learning_rate', 'batch_size', 'epochs', 'dropout', \
-    'optimizer', 'wv_len', \
-    'filter_sizes', 'filter_sets', 'num_filters', 'emb_l2', 'w_l2']
+file_path = os.path.dirname(os.path.realpath(__file__))
+lib_path2 = os.path.abspath(os.path.join(file_path, '..', '..', 'common'))
+sys.path.append(lib_path2)
 
 
-class BenchmarkP3B3(candle.Benchmark):
+REQUIRED = [
+    'learning_rate', 
+    'learning_rate_min',
+    'momentum',
+    'weight_decay',
+    'grad_clip',
+    'seed',
+    'unrolled',
+    'batch_size', 
+    'epochs', 
+]
+
+
+class BenchmarkP3B5(candle.Benchmark):
+    """ Benchmark for P3B5 """
 
     def set_locals(self):
-        """Functionality to set variables specific for the benchmark
-        - required: set of required parameters for the benchmark.
-        - additional_definitions: list of dictionaries describing the additional parameters for the
-        benchmark.
-        """
+        """ Set parameters for the benchmark.
 
-        if required is not None:
-            self.required = set(required)
-        # if additional_definitions is not None:
-            # self.additional_definitions = additional_definitions
+        Args: 
+            required: set of required parameters for the benchmark.
+        """
+        if REQUIRED is not None:
+            self.required = set(REQUIRED)
--- Pilot3/P3B5/p3b5_darts.py
+++ Pilot3/P3B5/p3b5_darts.py
@@ -30,36 +30,6 @@ lib_path2 = os.path.abspath(os.path.join(file_path, '..', '..', 'common'))
 sys.path.append(lib_path2)
 
 
-def initialize_parameters():
-    """ Initialize the parameters for the P3B5 benchmark """
-
-    p3b5_bench = bmk.BenchmarkP3B3(
-        bmk.file_path,
-        'p3b5_default_model.txt',
-        'pytorch',
-        prog='p3b5_baseline',
-        desc='Differentiable Architecture Search - Pilot 3 Benchmark 5',
-    )
-
-    # Initialize parameters
-    gParameters = candle.initialize_parameters(p3b5_bench)
-    #bmk.logger.info('Params: {}'.format(gParameters))
-
-    return gParameters
-
-
-def fetch_data(gParameters):
-    """ Downloads and decompresses the data if not locally available.
-        Since the training data depends on the model definition it is not loaded,
-        instead the local path where the raw data resides is returned
-    """
-
-    path = gParameters['data_url']
-    fpath = candle.fetch_file(path + gParameters['train_data'], 'Pilot3', untar=True)
-
-    return fpath
-
-
 def parse_args():
     parser = argparse.ArgumentParser(description='P3B3 Darts Example')
     parser.add_argument('--batch_size', type=int, default=100, metavar='N',
