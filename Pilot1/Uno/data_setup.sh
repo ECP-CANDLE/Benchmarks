fbee381dd5ffcc921df486fead5d673d17e8e081
brettin <brettin@anl.gov>
2020-01-08 08:50:46 -0500

    fixed path

--- Pilot1/Uno/data_setup.sh
+++ Pilot1/Uno/data_setup.sh
@@ -4,10 +4,10 @@ for n in $(seq -w 0 99) ; do
    mkdir -p $n
    pushd $n
    # Generate 1fold_$n_tr_id.csv and 1fold_$n_vl_id.csv
-   python data_split.py --seed $r --output 1fold_$n
+   python ../data_split.py --seed $r --output 1fold_$n
 
   # Generate h5 train validation splits
-  python topN_to_uno.py \
+  python ../topN_to_uno.py \
        --dataframe_from ../top_21.res_reg.cf_rnaseq.dd_dragon7.labeled.hdf5 \
        --fold 1fold_$n \
        --output top_21_1fold_$n.h5
