0fbe1dc54516592527031c53a07023a69b18b260
brettin <brettin@anl.gov>
2020-02-20 08:53:38 -0500

    make jsrun resource set aware for summit

--- Pilot1/Attn1/attn_bin_working_jan7_h5.sh
+++ Pilot1/Attn1/attn_bin_working_jan7_h5.sh
@@ -4,30 +4,45 @@ prefix="/gpfs/alpine/scratch/brettin/med106"
 local_prefix="/mnt/bb/$USER"
 
 m=$1
-echo $m
+datadir=$2
+
+echo "input arg file: $m"
+echo "input datadir: $datadir"
 
 for i in $(cat $m) ; do
+
   device=$(($i % 6))
-  # n="0$i"
-  n="00$i"
+
+  # pad with zeros to conform to input file names
+  if [ $i -lt 10 ] ; then
+    n=00"$i"
+  else
+    n=0"$i"
+  fi
 
   export CUDA_VISIBLE_DEVICES=$device
-  mkdir -p "$prefix"/save/"$n"
-  mkdir -p "$local_prefix"/save/"$n"
-  mkdir -p "$local_prefix"/top21_baseline
 
-  echo "copying files to $local_prefix/top21_baseline"
+  # should test if JSM_GPU_ASSIGNMENTS is empty
+  if [ $JSM_GPU_ASSIGNMENTS -eq $device ] ; then
+    echo "processing line value $i from infile $m using device $device on input $n"
+    mkdir -p "$prefix"/save/"$datadir"/"$n"
+    mkdir -p "$local_prefix"/save/"$datadir"/"$n"
+    mkdir -p "$local_prefix"/"$datadir"
 
-  cp "$prefix"/Data_sets/top21_baseline/top_21_1fold_"$n".h5 \
-     $local_prefix/top21_baseline/
+    echo "copying files to $local_prefix/$datadir"
+    cp "$prefix"/Data_sets/"$datadir"/top_21_1fold_"$n".h5 \
+       $local_prefix/"$datadir"/
 
-  ls $local_prefix/top21_baseline
+    ls $local_prefix/"$datadir"
 
-  echo "running attn_bin_working_jan7_h5.py --in $local_prefix/top21_baseline/top_21_1fold_"$n".h5"
-  python attn_bin_working_jan7_h5.py --in $local_prefix/top21_baseline/top_21_1fold_"$n".h5  \
-	--ep 2   \
-	--save_dir "$local_prefix"/save/"$n"/  > "$local_prefix"/save/"$n".log &
-	sleep 2
+    echo "running attn_bin_working_jan7_h5.py --in $local_prefix/$datadir/top_21_1fold_"$n".h5"
+    python attn_bin_working_jan7_h5.py --in $local_prefix/"$datadir"/top_21_1fold_"$n".h5  \
+        --ep 200  \
+        --save_dir "$local_prefix"/save/"$datadir"/"$n"/  > "$local_prefix"/save/"$datadir"/"$n".log &
+    sleep 2
+
+  fi
+
 done
 
 wait
