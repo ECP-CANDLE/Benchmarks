7e69547b73a8f2479214baa86c3d767cbafaba2b
Rajeev Jain <rajeeja@gmail.com>
2020-02-26 12:56:27 -0600

    First pass at making P1: Attn1 candle compliant

---
+++ Pilot1/Attn1/attn.py
@@ -0,0 +1,268 @@
+from __future__ import print_function
+
+import os
+import sys
+import logging
+
+import pandas as pd
+import numpy as np
+
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from scipy.stats.stats import pearsonr
+
+file_path = os.path.dirname(os.path.realpath(__file__))
+#lib_path = os.path.abspath(os.path.join(file_path, '..'))
+#sys.path.append(lib_path)
+lib_path2 = os.path.abspath(os.path.join(file_path, '..', '..', 'common'))
+sys.path.append(lib_path2)
+
+import candle
+
+logger = logging.getLogger(__name__)
+candle.set_parallelism_threads()
+
+additional_definitions = [ 
+{'name':'latent_dim', 
+    'action':'store',
+    'type': int, 
+    'help':'latent dimensions'},
+{'name':'model', 
+    'default':'ae',
+    'choices':['ae', 'vae', 'cvae'],
+    'help':'model to use: ae, vae, cvae'},
+{'name':'use_landmark_genes', 
+    'type': candle.str2bool,
+    'default': False,
+    'help':'use the 978 landmark genes from LINCS (L1000) as expression features'},
+{'name':'residual', 
+    'type': candle.str2bool,
+    'default': False,
+    'help':'add skip connections to the layers'},
+{'name':'reduce_lr', 
+    'type': candle.str2bool,
+    'default': False,
+    'help':'reduce learning rate on plateau'},
+{'name':'warmup_lr', 
+    'type': candle.str2bool,
+    'default': False,
+    'help':'gradually increase learning rate on start'},
+{'name':'base_lr', 
+    'type': float,
+    'help':'base learning rate'},
+{'name':'epsilon_std', 
+    'type': float,
+    'help':'epsilon std for sampling latent noise'},
+{'name':'cp', 
+    'type': candle.str2bool,
+    'default': False, 
+    'help':'checkpoint models with best val_loss'},
+#{'name':'shuffle', 
+    #'type': candle.str2bool,
+    #'default': False, 
+    #'help':'shuffle data'},
+{'name':'tb', 
+    'type': candle.str2bool,
+    'default': False, 
+    'help':'use tensorboard'},
+{'name':'tsne', 
+    'type': candle.str2bool,
+    'default': False, 
+    'help':'generate tsne plot of the latent representation'}
+]
+
+required = [
+    'activation',
+    'batch_size',
+    'dense',
+    'drop',
+    'epochs',
+    'initialization',
+    'learning_rate',
+    'loss',
+    'noise_factor',
+    'optimizer',
+    'rng_seed',
+    'model',
+    'scaling',
+    'validation_split',
+    'latent_dim',
+    'feature_subsample',
+    'batch_normalization',
+    'epsilon_std',
+    'solr_root',
+    'timeout'
+    ]
+
+class BenchmarkAttn(candle.Benchmark):
+
+    def set_locals(self):
+        """Functionality to set variables specific for the benchmark
+        - required: set of required parameters for the benchmark.
+        - additional_definitions: list of dictionaries describing the additional parameters for the
+        benchmark.
+        """
+
+        if required is not None:
+            self.required = set(required)
+        if additional_definitions is not None:
+            self.additional_definitions = additional_definitions
+
+def extension_from_parameters(params, framework=''):
+    """Construct string for saving model with annotation of parameters"""
+    ext = framework
+    ext += '.{}'.format(params['model'])
+    for i, n in enumerate(params['dense']):
+        if n:
+            ext += '.D{}={}'.format(i+1, n)
+    ext += '.A={}'.format(params['activation'])
+    ext += '.B={}'.format(params['batch_size'])
+    ext += '.E={}'.format(params['epochs'])
+    ext += '.L={}'.format(params['latent_dim'])
+    ext += '.LR={}'.format(params['learning_rate'])
+    ext += '.S={}'.format(params['scaling'])
+    if params['epsilon_std'] != 1.0:
+        ext += '.EPS={}'.format(params['epsilon_std'])
+    if params['drop']:
+        ext += '.DR={}'.format(params['drop'])
+    if params['batch_normalization']:
+        ext += '.BN'
+    if params['warmup_lr']:
+        ext += '.WU_LR'
+    if params['reduce_lr']:
+        ext += '.Re_LR'
+    if params['residual']:
+        ext += '.Res'
+
+    return ext
+def load_data(params, seed):
+
+    # start change #
+    if params['in'].endswith('h5') or params['in'].endswith('hdf5'):
+        print ('processing h5 in file {}'.format(params['in']))
+
+        df_x_train_0 = pd.read_hdf(params['in'], 'x_train_0').astype(np.float32)
+        df_x_train_1 = pd.read_hdf(params['in'], 'x_train_1').astype(np.float32)
+        X_train = pd.concat([df_x_train_0, df_x_train_1], axis=1, sort=False)
+        del df_x_train_0, df_x_train_1
+
+        df_x_test_0 = pd.read_hdf(params['in'], 'x_test_0').astype(np.float32)
+        df_x_test_1 = pd.read_hdf(params['in'], 'x_test_1').astype(np.float32)
+        X_test = pd.concat([df_x_test_0, df_x_test_1], axis=1, sort=False)
+        del df_x_test_0, df_x_test_1
+
+        df_x_val_0 = pd.read_hdf(params['in'], 'x_val_0').astype(np.float32)
+        df_x_val_1 = pd.read_hdf(params['in'], 'x_val_1').astype(np.float32)
+        X_val = pd.concat([df_x_val_0, df_x_val_1], axis=1, sort=False)
+        del df_x_val_0, df_x_val_1
+
+        Y_train = pd.read_hdf(params['in'], 'y_train')
+        Y_test = pd.read_hdf(params['in'], 'y_test')
+        Y_val = pd.read_hdf(params['in'], 'y_val')
+        
+        # assumes AUC is in the third column at index 2
+        # df_y = df['AUC'].astype('int')
+        # df_x = df.iloc[:,3:].astype(np.float32)
+
+        # assumes dataframe has already been scaled
+        # scaler = StandardScaler()
+        # df_x = scaler.fit_transform(df_x)
+
+    else:
+        print ('expecting in file file suffix h5')
+        sys.exit()
+    
+    
+    print('x_train shape:', X_train.shape)
+    print('x_test shape:', X_test.shape)
+    
+    return X_train, Y_train, X_val, Y_val, X_test, Y_test
+
+    # start change #
+    if params['in'].endswith('h5') or params['in'].endswith('hdf5'):
+        print ('processing h5 in file {}'.format(params['in']))
+
+        df_x_train_0 = pd.read_hdf(params['in'], 'x_train_0').astype(np.float32)
+        df_x_train_1 = pd.read_hdf(params['in'], 'x_train_1').astype(np.float32)
+        X_train = pd.concat([df_x_train_0, df_x_train_1], axis=1, sort=False)
+        del df_x_train_0, df_x_train_1
+
+        df_x_test_0 = pd.read_hdf(params['in'], 'x_test_0').astype(np.float32)
+        df_x_test_1 = pd.read_hdf(params['in'], 'x_test_1').astype(np.float32)
+        X_test = pd.concat([df_x_test_0, df_x_test_1], axis=1, sort=False)
+        del df_x_test_0, df_x_test_1
+
+        df_x_val_0 = pd.read_hdf(params['in'], 'x_val_0').astype(np.float32)
+        df_x_val_1 = pd.read_hdf(params['in'], 'x_val_1').astype(np.float32)
+        X_val = pd.concat([df_x_val_0, df_x_val_1], axis=1, sort=False)
+        del df_x_val_0, df_x_val_1
+
+        Y_train = pd.read_hdf(params['in'], 'y_train')
+        Y_test = pd.read_hdf(params['in'], 'y_test')
+        Y_val = pd.read_hdf(params['in'], 'y_val')
+        
+        # assumes AUC is in the third column at index 2
+        # df_y = df['AUC'].astype('int')
+        # df_x = df.iloc[:,3:].astype(np.float32)
+
+        # assumes dataframe has already been scaled
+        # scaler = StandardScaler()
+        # df_x = scaler.fit_transform(df_x)
+
+    else:
+        print ('expecting in file file suffix h5')
+        sys.exit()
+    
+    
+    print('x_train shape:', X_train.shape)
+    print('x_test shape:', X_test.shape)
+    
+    return X_train, Y_train, X_val, Y_val, X_test, Y_test
+
+
+
+
+
+def load_data_orig(params, seed):
+    if params['with_type']:
+        drop_cols = ['case_id']
+        onehot_cols = ['cancer_type']
+    else:
+        drop_cols = ['case_id', 'cancer_type']
+        onehot_cols = None
+
+    if params['use_landmark_genes']:
+        lincs_file = 'lincs1000.tsv'
+        lincs_path = candle.fetch_file(url_p1b1 + lincs_file)
+        df_l1000 = pd.read_csv(lincs_path, sep='\t')
+        usecols = df_l1000['gdc']
+        drop_cols = None
+    else:
+        usecols = None
+
+    return candle.load_X_data(params['url_attn'], params['file_train'], params['file_test'],
+                                 drop_cols=drop_cols,
+                                 onehot_cols=onehot_cols,
+                                 usecols=usecols,
+                                 n_cols=params['feature_subsample'],
+                                 shuffle=params['shuffle'],
+                                 scaling=params['scaling'],
+                                 validation_split=params['validation_split'],
+                                 dtype=params['datatype'],
+                                 seed=seed)
+
+
+def evaluate_autoencoder(y_pred, y_test):
+    try:
+        mse = mean_squared_error(y_pred, y_test)
+        r2 = r2_score(y_test, y_pred)
+        corr, _ = pearsonr(y_pred.flatten(), y_test.flatten())
+        # print('Mean squared error: {}%'.format(mse))
+    except:
+        #when nan or something else breaks mean_squared_error computation
+        # we may check earlier before computation also:
+        #np.isnan(y_pred).any() or np.isnan(y_test).any()):
+        r2 = 0
+        mse = 0
+        corr = 0
+    return {'mse': mse, 'r2_score': r2, 'correlation': corr}
