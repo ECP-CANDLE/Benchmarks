6ccb4f3c78bda3ceaf6cd3ef476c76e98e7b7e1f
Todd Young <young.todd.mk@gmail.com>
2020-04-01 10:20:58 -0400

    Add utils
    This makes handling device placement a bit easier and gives us the
    ability to log a single task accuracy.

--- Pilot3/P3B5/darts/utils/logging.py
+++ Pilot3/P3B5/darts/utils/logging.py
@@ -25,3 +25,21 @@ def log_accuracy(accuracy, split: str='train'):
     )
 
     logger.info(acc_info)
+
+
+def log_single_accuracy(accuracy, split: str='train'):
+    """ Log the average accuracy for a single task
+
+    Parameters
+    ----------
+    accuracy: darts.MultitaskAccuracyMeter
+        Current accuracy meter state
+
+    split: str
+        Either training of testing
+    """
+    acc_info = (
+        f">>> {split.upper()} Accuracy - Response: {accuracy.get_avg_accuracy('response'):.4f}, "
+    )
+
+    logger.info(acc_info)
