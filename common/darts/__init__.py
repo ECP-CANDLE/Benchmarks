3adb24b57b954d1c27277626d0a29f279a07cb7e
yngtodd <young.todd.mk@gmail.com>
2020-04-08 09:33:41 -0400

    Fix typo
    Just fixing import mispelling.

--- common/darts/__init__.py
+++ common/darts/__init__.py
@@ -3,7 +3,7 @@ __email__ = 'youngmt1@ornl.gov'
 __version__ = '0.1.0'
 
 from .architecture import Architecture
-from .modules.conv.network imoprt ConvNetwork
+from .modules.conv.network import ConvNetwork
 from .modules.linear.network import LinearNetwork
 
 
