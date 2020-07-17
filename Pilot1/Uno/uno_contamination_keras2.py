24827d04eb69265004e2f7f7c7405d6ca1c3fdb6
Cristina Garcia Cardona <cgarciac@lanl.gov>
2020-07-17 12:04:27 -0600

    updated descriptions in uno UQ scripts

--- Pilot1/Uno/uno_contamination_keras2.py
+++ Pilot1/Uno/uno_contamination_keras2.py
@@ -110,7 +110,7 @@ def initialize_parameters(default_model='uno_defaultUQ_model.txt'):
 
     # Build benchmark object
     unoBmk = benchmark.BenchmarkUno(benchmark.file_path, default_model, 'keras',
-                                    prog='uno_baseline', desc='Build neural network based models to predict tumor response to single and paired drugs. Use contamination model for detecting outilers in training.')
+                                    prog='uno_contamination', desc='Build neural network based models to predict tumor response to single and paired drugs. Use contamination model for detecting outliers in training.')
 
     # update locals
     unoBmk.required.update(required)
