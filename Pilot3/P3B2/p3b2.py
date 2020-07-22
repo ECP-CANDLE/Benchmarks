cdd149577505e94b948c49dba9b9f5198447b2c4
Jamal <jamal@lanl.gov>
2020-07-22 12:58:59 -0600

    P3B2 pep8 style cleanup.

--- Pilot3/P3B2/p3b2.py
+++ Pilot3/P3B2/p3b2.py
@@ -2,7 +2,6 @@ from __future__ import print_function
 
 import os
 import sys
-import argparse
 
 file_path = os.path.dirname(os.path.realpath(__file__))
 lib_path2 = os.path.abspath(os.path.join(file_path, '..', '..', 'common'))
@@ -10,40 +9,42 @@ sys.path.append(lib_path2)
 
 import candle
 
-additional_definitions = [ 
-{'name':'rnn_size', 
-    'action':'store',
-    'type':int,
-    'help':'size of LSTM internal state'},
-{'name':'n_layers', 
-    'action':'store',
-    'help':'number of layers in the LSTM'}, 
-{'name':'do_sample', 
-    'type':candle.str2bool,
-    'help':'generate synthesized text'},
-{'name':'temperature', 
-    'action':'store',
-    'type': float,
-    'help':'variability of text synthesis'},
-{'name':'primetext', 
-    'action':'store',
-    'help': 'seed string of text synthesis' },
-{'name':'length', 
-    'action':'store',
-    'type': int,
-    'help': 'length of synthesized text'},
+additional_definitions = [
+    {'name': 'rnn_size',
+     'action': 'store',
+     'type': int,
+     'help': 'size of LSTM internal state'},
+    {'name': 'n_layers',
+     'action': 'store',
+     'help': 'number of layers in the LSTM'},
+    {'name': 'do_sample',
+     'type': candle.str2bool,
+     'help': 'generate synthesized text'},
+    {'name': 'temperature',
+     'action': 'store',
+     'type': float,
+     'help': 'variability of text synthesis'},
+    {'name': 'primetext',
+     'action': 'store',
+     'help': 'seed string of text synthesis'},
+    {'name': 'length',
+     'action': 'store',
+     'type': int,
+     'help': 'length of synthesized text'},
 ]
 
-required = ['train_data', 'rnn_size', 'epochs', 'n_layers', \
-        'learning_rate', 'dropout', 'recurrent_dropout', \
-        'temperature','primetext', 'length']
+required = ['train_data', 'rnn_size', 'epochs', 'n_layers',
+            'learning_rate', 'dropout', 'recurrent_dropout',
+            'temperature', 'primetext', 'length']
+
 
 class BenchmarkP3B2(candle.Benchmark):
 
     def set_locals(self):
         """Functionality to set variables specific for the benchmark
         - required: set of required parameters for the benchmark.
-        - additional_definitions: list of dictionaries describing the additional parameters for the
+        - additional_definitions: list of dictionaries \
+              describing the additional parameters for the
         benchmark.
         """
 
