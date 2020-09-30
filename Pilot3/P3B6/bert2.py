49e7ae2403fe0dda63a6ff4aeb62580e01d3d977
yngtodd <young.todd.mk@gmail.com>
2020-09-30 17:23:10 -0400

    Add new data

---
+++ Pilot3/P3B6/bert2.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+from pytorch_pretrained_bert import (
+    BertForSequenceClassification
+)
+
+
+class HiBERT(nn.Module):
+
+    def __init__(self, bert_load_path, num_classes):
+
+        super(HiBERT, self).__init__()
+
+        self.bert = BertForSequenceClassification.from_pretrained(
+            bert_load_path, num_labels=num_classes
+        )
+
+    def forward(self, input_ids, input_mask, segment_ids, n_segs):
+        logits = self.bert(input_ids, input_mask, segment_ids, labels=None)
+        logits = torch.max(logits, 0)[0]
+        return logits
+
