7f1304577460e2e00fc957fed20bd205a8b1a301
Jamal <jamal@lanl.gov>
2021-02-19 10:10:19 -0700

    Add new keywords.

--- common/keras_utils.py
+++ common/keras_utils.py
@@ -258,5 +258,3 @@ class LoggingCallback(Callback):
     def on_epoch_end(self, epoch, logs={}):
         msg = "[Epoch: %i] %s" % (epoch, ", ".join("%s: %f" % (k, v) for k, v in sorted(logs.items())))
         self.print_fcn(msg)
-
-
