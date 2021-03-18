9bdf88036e5b32040f20587f0d74134b08220675
Harry Yoo <hsyoo@anl.gov>
2021-03-18 13:00:45 -0500

    add rnn generator example

---
+++ examples/rnngen/fetch_models.sh
@@ -0,0 +1,7 @@
+cd mosesrun
+curl https://ftp.mcs.anl.gov/pub/candle/public/benchmarks/Examples/rnngen/mosesrun/autosave.model.pt -o autosave.model.pt
+cd -
+cd pilot1
+curl https://ftp.mcs.anl.gov/pub/candle/public/benchmarks/Examples/rnngen/pilot1/ft_goodperforming_model.pt -o ft_goodperforming_model.pt
+curl https://ftp.mcs.anl.gov/pub/candle/public/benchmarks/Examples/rnngen/pilot1/ft_poorperforming_model.pt -o ft_poorperforming_model.pt
+cd -
