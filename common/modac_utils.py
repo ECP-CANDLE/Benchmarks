6ff1abd344971abb394b79af17a42614342472b6
Jamal Mohd Yusof <jamal@lanl.gov>
2021-04-21 11:04:22 -0600

    Fix typos, codestyle issues.

--- common/modac_utils.py
+++ common/modac_utils.py
@@ -4,6 +4,11 @@ import requests
 from tqdm import tqdm
 import os
 
+
+modac_user = None
+modac_pass = None
+
+
 def get_file_from_modac(fname, origin):
     """ Downloads a file from the "Model and Data Clearning House" (MoDAC)
     repository. Users should already have a MoDAC account to download the data.
@@ -23,15 +28,15 @@ def get_file_from_modac(fname, origin):
     """
     print('Downloading data from modac.cancer.gov, make sure you have an account first.')
     total_size_in_bytes = get_dataObject_modac_filesize(origin)
-    modac_user, modac_token = authenticate_modac()
+
+    auth = authenticate_modac()
     data = json.dumps({})
-    headers = {} 
-    headers["Content-Type"] = "application/json"
-    headers["Authorization"] = "Bearer {0}".format(modac_token)
+    headers = {'Content-Type': 'application/json'}
+    auth = (modac_user, modac_pass)
 
     post_url = origin + '/download'
     print("Downloading: " + post_url + " ...")
-    response = requests.post(post_url, data = data, headers = headers, stream = True)
+    response = requests.post(post_url, data=data, headers=headers, auth=auth, stream=True)
     if response.status_code != 200:
         print("Error downloading from modac.cancer.gov")
         raise Exception("Response code: {0}, Response message: {1}".format(response.status_code, response.text))
@@ -48,148 +53,27 @@ def get_file_from_modac(fname, origin):
     print('Saved file to: ' + fname)
     return fname
 
-def register_file_to_modac(file_path, metadata, destination_path):
-    """ Register a file in the "Model and Data Clearning House" (MoDAC)
 
-        Parameters
-        ----------
-        file_path : string
-            path on disk for the file to be uploaded 
-        metadata: dictionary 
-            dictionary of attribute/value pairs of metadata to associate with
-            the file in MoDaC
-        destination : string
-            The path on MoDaC in form of collection/filename  
-
-        Returns
-        ----------
-        integer 
-            The returned code from the PUT request 
-    """
-    print('Registering the file {0} at MoDaC location:{1}'.format(file_path, destination_path))
-    register_url = "https://modac.cancer.gov/api/v2/dataObject/" + destination_path 
-
-
-    formated_metadata = [ dict([("attribute", attribute), ("value",metadata[attribute])]) for attribute in metadata.keys() ]
-    metadata_dict={"metadataEntries":formated_metadata} 
-
-    # Based on: https://www.tutorialspoint.com/requests/requests_file_upload.htm
-    files = {}
-    files['dataObjectRegistration'] = ('attributes', json.dumps(metadata_dict),"application/json")
-    files["dataObject"] = (file_path, open(file_path, 'rb'))
-    modac_user, modac_token = authenticate_modac()
-    headers = {} 
-    headers["Authorization"] = "Bearer {0}".format(modac_token)
-
-    response = requests.put(register_url, headers = headers, files=files)
-    if response.status_code != 200:
-        print(response.headers)
-        print(response.text)
-        print("Error registering file to modac.cancer.gov")
-        raise Exception("Response code: {0}, Response message: {1}".format(response.status_code, response.text))
-
-    print(response.text, response.status_code)
-    return response.status_code 
-
-def authenticate_modac(generate_token=False): 
+def authenticate_modac():
     """
     Authenticates a user on modac.cancer.gov
-
-        Parameters
-        ----------
-        generate_token : Bool  
-            Either generate a new token, or read saved token if it exists 
- 
-        
         Returns
         ----------
         tuple(string,string)
             tuple with the modac credentials
     """
 
-    from os.path import expanduser
-    home = expanduser("~")
-    modac_token_dir =  os.path.abspath(os.path.join(home,".nci-modac"))
-    modac_token_file = "credentials.json"
-    user_attr = "modac_user"
-    token_attr = "modac_token"
-    modac_token_path = os.path.join(modac_token_dir, modac_token_file)
-    credentials_dic = {}  
-
-    if  not generate_token and os.path.exists(modac_token_path):
-        with open(modac_token_path) as f:
-          credentials_dic = json.load(f)
-
-    else:
-        #Get credentials 
+    global modac_user
+    global modac_pass
+    if modac_user is None:
         modac_user = input("MoDaC Username: ")
+
+    if modac_pass is None:
         import getpass
         modac_pass = getpass.getpass("MoDaC Password: ")
 
-        #Generate token
-        auth = (modac_user, modac_pass)
-        auth_url = 'https://modac.cancer.gov/api/authenticate'
-        print("Authenticating " + modac_user + " ...")
-        response = requests.get(auth_url, auth = auth, stream = True)
-        if response.status_code != 200:
-            print("Error authenticating modac user:{0}", modac_user)
-            raise Exception("Response code: {0}, Response message: {1}".format(response.status_code, response.text))
+    return (modac_user, modac_pass)
 
-        else: 
-            token = response.text
-            if not os.path.exists(modac_token_path):
-                save_question = "Save MoDaC token in {0}".format(modac_token_path)
-                save_token = query_yes_no(save_question)
-            else: 
-                save_token = True
-            if save_token: 
-                if not os.path.isdir(modac_token_dir):
-                    os.mkdir(modac_token_dir)
-                credentials_dic[user_attr] = modac_user 
-                credentials_dic[token_attr] =  token
-                with open(modac_token_path, "w") as outfile: 
-                    json.dump(credentials_dic, outfile, indent=4)
-                 
-    return (credentials_dic[user_attr], credentials_dic[token_attr])
-    
-def query_yes_no(question, default="yes"):
-    """
-    Ask a yes/no question via raw_input() and return their answer.
-    
-        Parameters
-        ----------
-        question: string 
-            string that is presented to the user.
-        default: boolean
-            The presumed boolean answer if the user just hits <Enter>.
-        
-        Returns
-        ----------
-        boolean  
-            True for "yes" or False for "no".
-
-    """
-    valid = {"yes": True, "y": True, "ye": True,
-             "no": False, "n": False}
-    if default is None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("invalid default answer: '%s'" % default)
-
-    while True:
-        print(question + prompt)
-        choice = input().lower()
-        if default is not None and choice == '':
-            return valid[default]
-        elif choice in valid:
-            return valid[choice]
-        else:
-            print("Please respond with 'yes' or 'no' "
-                             "(or 'y' or 'n').\n")
 
 def get_dataObject_modac_filesize(data_object_path):
     """
@@ -224,7 +108,7 @@ def get_dataObject_modac_md5sum(data_object_path):
         string
             The md5sum of the file
     """
-    self_dic = get_dataObject_dme_meta(data_object_path)
+    self_dic = get_dataObject_modac_meta(data_object_path)
     if "checksum" in self_dic.keys():
         return self_dic["checksum"]
     else:
@@ -244,11 +128,10 @@ def get_dataObject_modac_meta(data_object_path):
         dictionary
             Dictonary of all metadata for the file in MoDAC
     """
-    #data_object_path = encode_path(data_object_path)
-    modac_user, modac_token  = authenticate_modac()
-    headers = {} 
-    headers["Authorization"] = "Bearer {0}".format(modac_token)
-    get_response = requests.get(data_object_path, headers = headers)
+    # data_object_path = encode_path(data_object_path)
+    auth = authenticate_modac()
+
+    get_response = requests.get(data_object_path, auth=auth)
     if get_response.status_code != 200:
         print("Error downloading from modac.cancer.gov", data_object_path)
         raise Exception("Response code: {0}, Response message: {1}".format(get_response.status_code, get_response.text))
@@ -260,13 +143,3 @@ def get_dataObject_modac_meta(data_object_path):
         self_dic[pair['attribute']] = pair['value']
 
     return self_dic
-
-if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-a', '--authenticate', action = 'store_true', 
-                        help='Authenticate MoDaC user and create token')
-
-    args = parser.parse_args()
-    if args.authenticate:
-        authenticate_modac(generate_token=True)
