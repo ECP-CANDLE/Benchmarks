2c5d83d9e86dac74b7e6462d93ad3dbe24806952
Jamal <jamal@lanl.gov>
2021-05-24 11:29:31 -0600

    Fix linter issues.

--- examples/histogen/sample_baseline_pytorch.py
+++ examples/histogen/sample_baseline_pytorch.py
@@ -97,11 +97,12 @@ def sample_model(model, device, batch, size, temperature, condition=None):
 
     return row
 
+
 def get_data(gParams):
     data_url = gParams['data_url']
-    gParams['vqvae'] = candle.fetch_file(data_url+gParams['vqvae'], subdir = 'Examples/histogen')
-    gParams['top'] = candle.fetch_file(data_url+gParams['top'], subdir = 'Examples/histogen')
-    gParams['bottom'] = candle.fetch_file(data_url+gParams['bottom'], subdir = 'Examples/histogen')
+    gParams['vqvae'] = candle.fetch_file(data_url + gParams['vqvae'], subdir='Examples/histogen')
+    gParams['top'] = candle.fetch_file(data_url + gParams['top'], subdir='Examples/histogen')
+    gParams['bottom'] = candle.fetch_file(data_url + gParams['bottom'], subdir='Examples/histogen')
 
 
 def load_model(model, checkpoint, device):
--- examples/histogen/train_vqvae_baseline_pytorch.py
+++ examples/histogen/train_vqvae_baseline_pytorch.py
@@ -55,7 +55,6 @@ required = [
     'epochs',
     'learning_rate',
     'sched_mode',
-    #'data_dir',
     'size',
 ]
 
@@ -211,9 +210,9 @@ def config_and_train(args):
 def fetch_data(params):
     data_url = params['data_url']
     if params['data_dir'] is None:
-        params['data_dir'] = candle.fetch_file(data_url+params['train_data'], subdir = 'Examples/histogen')
+        params['data_dir'] = candle.fetch_file(data_url + params['train_data'], subdir='Examples/histogen')
     else:
-        tempfile = candle.fetch_file(data_url+params['train_data'], subdir = 'Examples/histogen')
+        tempfile = candle.fetch_file(data_url + params['train_data'], cache_subdir='Examples/histogen')
         params['data_dir'] = os.path.join(os.path.dirname(tempfile), params['data_dir'])
 
 
