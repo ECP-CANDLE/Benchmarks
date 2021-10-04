48a4b451c37cb17fda05566d84f8d143c2693024
Cristina Garcia Cardona <cgarciac@pn2006146.lanl.gov>
2021-10-04 12:57:47 -0600

    added preliminary candle template to p3b9

---
+++ Pilot3/P3B9/p3b9.py
@@ -0,0 +1,74 @@
+import os
+import sys
+
+file_path = os.path.dirname(os.path.realpath(__file__))
+lib_path2 = os.path.abspath(os.path.join(file_path, '..', '..', 'common'))
+sys.path.append(lib_path2)
+
+import candle
+
+additional_definitions = [
+    {'name': 'per_device_train_batch_size',
+        'type': int,
+        'default': 16,
+        'help': 'Batch per device in training'},
+    {'name': 'gradient_accumulation_steps',
+        'type': int,
+        'default': 1,
+        'help': 'Number of steps for accumulating gradient'},
+    {'name': 'max_len',
+        'type': int,
+        'default': 512,
+        'help': 'Max length for'},
+    {'name': 'weight_decay',
+        'type': float,
+        'default': 0.0000,
+        'help': 'ADAM weight decay'},
+    {'name': 'adam_beta2',
+        'type': float,
+        'default': 0.98,
+        'help': 'ADAM beta2 parameter'},
+    {'name': 'adam_epsilon',
+        'type': float,
+        'default': 2e-8,
+        'help': 'ADAM epsilon parameter'},
+    {'name': 'max_steps',
+        'type': int,
+        'default': 10,
+        'help': 'Max training steps'},
+    {'name': 'warmup_steps',
+        'type': int,
+        'default': 1,
+        'help': 'Warmup steps'},
+]
+
+required = [
+    'output_dir',
+    'train_bool',
+    'per_device_train_batch_size',
+    'gradient_accumulation_steps',
+    'max_len',
+    'learning_rate',
+    'weight_decay',
+    'adam_beta2',
+    'adam_epsilon'
+    'max_steps',
+    'warmup_steps',
+]
+
+
+class BenchmarkP3B9(candle.Benchmark):
+    """ Benchmark for BERT """
+
+    def set_locals(self):
+        """ Set parameters for the benchmark.
+
+        Args:
+            required: set of required parameters for the benchmark.
+            additional_definitions: list of dictionaries describing the additional parameters for the
+            benchmark.
+        """
+        if required is not None:
+            self.required = set(required)
+        if additional_definitions is not None:
+            self.additional_definitions = additional_definitions
---
+++ Pilot3/P3B9/p3b9_baseline_pytorch.py
@@ -0,0 +1,103 @@
+import torch
+import p3b9 as bmk
+import candle
+
+from dataclasses import dataclass, field
+from transformers import (
+BertTokenizer, BertConfig, BertForMaskedLM, DataCollatorForLanguageModeling, Trainer, TrainingArguments, HfArgumentParser
+)
+
+import webdataset as wd
+
+
+@dataclass
+class ModelArguments:
+    """
+    Arguments pertaining tomodel/config/tokenizer
+    """
+    model_name_or_path: str = field(
+        default=None
+    )
+    savepath: str = field(
+        default="./pretrained"
+    )
+    max_len: int = field(
+        default=128
+    )
+
+
+class truncate(object):
+
+    def __init__(self, max_len):
+        self.max_len = max_len
+
+    def __call__(self, doc):
+        return doc[:self.max_len]
+
+
+def initialize_parameters():
+    """ Initialize the parameters for the P3B5 benchmark """
+
+    p3b9_bench = bmk.BenchmarkP3B9(
+        bmk.file_path,
+        "default_model.txt",
+        "pytorch",
+        prog="p3b9",
+        desc="BERT",
+    )
+
+    gParameters = candle.finalize_parameters(p3b9_bench)
+    return gParameters
+
+
+def run(args):
+
+    parser = HfArgumentParser((ModelArguments, TrainingArguments))
+    model_args, training_args = parser.parse_args_into_dataclasses()
+    print(model_args, training_args)
+    trunc = truncate(model_args.max_len)
+
+    data_len = 1000
+    print('total data len per gpu:', data_len)
+    dataset = wd.Dataset('part-000000.tar',
+                     length=data_len, shuffle=True).decode('torch').rename(input_ids='pth').map_dict(input_ids=trunc).shuffle(1000)
+
+    tokenizer = BertTokenizer.from_pretrained('pubmed_bert-vocab.txt')
+
+    data_collator = DataCollatorForLanguageModeling(
+        tokenizer=tokenizer, mlm=True, mlm_probability=0.15
+    )
+
+    config = BertConfig(
+        vocab_size=30_000,
+        hidden_size=768,
+        intermediate_size=3072,
+        max_position_embeddings=512,
+        num_attention_heads=12,
+        num_hidden_layers=12,
+        type_vocab_size=2,
+    )
+
+    if model_args.model_name_or_path is not None:
+        model = BertForMaskedLM.from_pretrained(model_args.model_name_or_path)
+    else:
+        model = BertForMaskedLM(config=config)
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        data_collator=data_collator,
+        train_dataset=dataset
+    )
+
+    trainer.train()
+    trainer.save_model(model_args.savepath)
+
+
+def main():
+    params = initialize_parameters()
+    run(params)
+
+
+if __name__ == "__main__":
+    main()
