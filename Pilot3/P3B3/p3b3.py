7b2b1a6a0b7eb2e737557f1aa124c4723705de34
Jamal <jamal@lanl.gov>
2022-05-11 15:01:46 -0600

    Update Pilot3 Benchmarks to use candle_lib

--- Pilot3/P3B3/p3b3.py
+++ Pilot3/P3B3/p3b3.py
@@ -1,11 +1,8 @@
 from __future__ import print_function
 
 import os
-import sys
 
 file_path = os.path.dirname(os.path.realpath(__file__))
-lib_path2 = os.path.abspath(os.path.join(file_path, '..', '..', 'common'))
-sys.path.append(lib_path2)
 
 import candle
 
--- Pilot3/P3B5/p3b5_darts.py
+++ Pilot3/P3B5/p3b5_darts.py
@@ -11,10 +11,6 @@ from torch.utils.data import DataLoader
 
 
 file_path = os.path.dirname(os.path.realpath(__file__))
-lib_path = os.path.abspath(os.path.join(file_path, '..'))
-sys.path.append(lib_path)
-lib_path2 = os.path.abspath(os.path.join(file_path, '..', '..', 'common'))
-sys.path.append(lib_path2)
 
 
 import darts
