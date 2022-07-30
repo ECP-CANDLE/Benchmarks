25d87570d61c8628add17a8b4bb2c1136ed7a6d9
Thomas Brettin <brettin@anl.gov>
2022-07-30 10:27:28 -0500

    namespace errors

--- examples/tf_distribute_example/multi_worker.py
+++ examples/tf_distribute_example/multi_worker.py
@@ -64,11 +64,11 @@ strategy = tf.distribute.MultiWorkerMirroredStrategy()
 logging.debug("done calling tf.distribute.MultiWorkerMirroredStrategy()")
 
 global_batch_size = per_worker_batch_size * num_workers
-multi_worker_dataset = mnist_setup.mnist_dataset(global_batch_size)
+multi_worker_dataset = mnist_dataset(global_batch_size)
 
 with strategy.scope():
   # Model building/compiling need to be within `strategy.scope()`.
-  multi_worker_model = mnist_setup.build_and_compile_cnn_model()
+  multi_worker_model = build_and_compile_cnn_model()
 
 
 multi_worker_model.fit(multi_worker_dataset, epochs=3, steps_per_epoch=70)
