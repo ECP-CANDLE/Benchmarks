98d7aabc189efd9f807ef5d2d2b92b6bc34fa6ba
brettin <brettin@anl.gov>
2022-07-30 13:08:11 -0500

    single node multi gpu

--- Pilot1/ST1/smiles_regress_transformer.py
+++ Pilot1/ST1/smiles_regress_transformer.py
@@ -1,64 +1,81 @@
-# Setup
+## Setup
 
-import argparse
-import os
-
-import matplotlib
 import pandas as pd
+import numpy as np
+import os
+import sys
+import gzip
+import argparse
 
-matplotlib.use("Agg")
+import math
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+
 
 import tensorflow as tf
+
 from tensorflow import keras
 from tensorflow.keras import backend as K
+from tensorflow.keras.optimizers import SGD, Adam, RMSprop
+from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger, ReduceLROnPlateau, EarlyStopping
 from tensorflow.keras import layers
-from tensorflow.keras.callbacks import (
-    CSVLogger,
-    EarlyStopping,
-    ModelCheckpoint,
-    ReduceLROnPlateau,
-)
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.preprocessing import sequence, text
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.preprocessing import text
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+#if gpus:
+#    try:
+#        for gpu in gpus:
+#            tf.config.experimental.set_memory_growth(gpu, True)
+#    except RuntimeError as e:
+#        print(e)
+
+# Load the TensorBoard notebook extension.
+strategy = tf.distribute.MirroredStrategy()
+print('tensorflow version: {}'.format(tf.__version__))
+print('Number of devices: {}'.format(strategy.num_replicas_in_sync))
+
 
 file_path = os.path.dirname(os.path.realpath(__file__))
+lib_path = os.path.abspath(os.path.join(file_path, '..', '..', 'common'))
+sys.path.append(lib_path)
 
-psr = argparse.ArgumentParser(description="input csv file")
-psr.add_argument("--in_train", default="in_train")
-psr.add_argument("--in_vali", default="in_vali")
-psr.add_argument("--ep", type=int, default=400)
-args = vars(psr.parse_args())
+
+psr = argparse.ArgumentParser(description='input csv file')
+psr.add_argument('--in_train',  default='in_train')
+psr.add_argument('--in_vali',  default='in_vali')
+psr.add_argument('--ep',  type=int, default=400)
+args=vars(psr.parse_args())
 print(args)
 
-EPOCH = args["ep"]
+EPOCH = args['ep']
 BATCH = 32
+BATCH = BATCH * strategy.num_replicas_in_sync
 
-data_path_train = args["in_train"]
-data_path_vali = args["in_vali"]
+data_path_train = args['in_train']
+data_path_vali = args['in_vali']
 
-DR = 0.1  # Dropout rate
+DR    = 0.1      # Dropout rate                  
 
-# define r2 for reporting
-
+### define r2 for reporting
 
 def r2(y_true, y_pred):
-    SS_res = K.sum(K.square(y_true - y_pred))
+    SS_res =  K.sum(K.square(y_true - y_pred))
     SS_tot = K.sum(K.square(y_true - K.mean(y_true)))
-    return 1 - SS_res / (SS_tot + K.epsilon())
+    return (1 - SS_res/(SS_tot + K.epsilon()))
 
-
-# Implement a Transformer block as a layer
-
+## Implement a Transformer block as a layer
 
 class TransformerBlock(layers.Layer):
     def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1):
         super(TransformerBlock, self).__init__()
         self.att = layers.MultiHeadAttention(num_heads=num_heads, key_dim=embed_dim)
         self.ffn = keras.Sequential(
-            [
-                layers.Dense(ff_dim, activation="relu"),
-                layers.Dense(embed_dim),
-            ]
+            [layers.Dense(ff_dim, activation="relu"), layers.Dense(embed_dim),]
         )
         self.layernorm1 = layers.LayerNormalization(epsilon=1e-6)
         self.layernorm2 = layers.LayerNormalization(epsilon=1e-6)
@@ -73,10 +90,8 @@ class TransformerBlock(layers.Layer):
         ffn_output = self.dropout2(ffn_output, training=training)
         return self.layernorm2(out1 + ffn_output)
 
-
-# Implement embedding layer
-# Two seperate embedding layers, one for tokens, one for token index (positions).
-
+## Implement embedding layer
+## Two seperate embedding layers, one for tokens, one for token index (positions).
 
 class TokenAndPositionEmbedding(layers.Layer):
     def __init__(self, maxlen, vocab_size, embed_dim):
@@ -92,10 +107,10 @@ class TokenAndPositionEmbedding(layers.Layer):
         return x + positions
 
 
-# Input and prepare dataset
+## Input and prepare dataset
 
-vocab_size = 40000  #
-maxlen = 250  #
+vocab_size = 40000  # 
+maxlen = 250  # 
 
 
 data_train = pd.read_csv(data_path_train)
@@ -111,12 +126,10 @@ y_val = data_vali["type"].values.reshape(-1, 1) * 1.0
 tokenizer = text.Tokenizer(num_words=vocab_size)
 tokenizer.fit_on_texts(data_train["smiles"])
 
-
 def prep_text(texts, tokenizer, max_sequence_length):
     # Turns text into into padded sequences.
     text_sequences = tokenizer.texts_to_sequences(texts)
     return sequence.pad_sequences(text_sequences, maxlen=maxlen)
-
 
 x_train = prep_text(data_train["smiles"], tokenizer, maxlen)
 x_val = prep_text(data_vali["smiles"], tokenizer, maxlen)
@@ -124,76 +137,64 @@ x_val = prep_text(data_vali["smiles"], tokenizer, maxlen)
 print(x_train.shape)
 print(y_train.shape)
 
-# Create regression/classifier model using N transformer layers
+## Create regression/classifier model using N transformer layers
 
 embed_dim = 128  # Embedding size for each token
 num_heads = 16  # Number of attention heads
 ff_dim = 128  # Hidden layer size in feed forward network inside transformer
 
-inputs = layers.Input(shape=(maxlen,))
-embedding_layer = TokenAndPositionEmbedding(maxlen, vocab_size, embed_dim)
-x = embedding_layer(inputs)
-transformer_block = TransformerBlock(embed_dim, num_heads, ff_dim)
-x = transformer_block(x)
-x = transformer_block(x)
-x = transformer_block(x)
-x = transformer_block(x)
+with strategy.scope():
+    inputs = layers.Input(shape=(maxlen,))
+    embedding_layer = TokenAndPositionEmbedding(maxlen, vocab_size, embed_dim)
+    x = embedding_layer(inputs)
+    transformer_block = TransformerBlock(embed_dim, num_heads, ff_dim)
+    x = transformer_block(x)
+    x = transformer_block(x)
+    x = transformer_block(x)
+    x = transformer_block(x)
 
-# x = layers.GlobalAveragePooling1D()(x)  --- the original model used this but the accuracy was much lower
+    #x = layers.GlobalAveragePooling1D()(x)  --- the original model used this but the accuracy was much lower
 
-x = layers.Reshape((1, 32000), input_shape=(250, 128,))(
-    x
-)  # reshaping increases parameters but improves accuracy a lot
-x = layers.Dropout(0.1)(x)
-x = layers.Dense(1024, activation="relu")(x)
-x = layers.Dropout(0.1)(x)
-x = layers.Dense(256, activation="relu")(x)
-x = layers.Dropout(0.1)(x)
-x = layers.Dense(64, activation="relu")(x)
-x = layers.Dropout(0.1)(x)
-x = layers.Dense(16, activation="relu")(x)
-x = layers.Dropout(0.1)(x)
-outputs = layers.Dense(1, activation="relu")(x)
+    x = layers.Reshape((1,32000), input_shape=(250,128,))(x)  # reshaping increases parameters but improves accuracy a lot
+    x = layers.Dropout(0.1)(x)
+    x = layers.Dense(1024, activation="relu")(x)
+    x = layers.Dropout(0.1)(x)
+    x = layers.Dense(256, activation="relu")(x)
+    x = layers.Dropout(0.1)(x)
+    x = layers.Dense(64, activation='relu')(x)
+    x = layers.Dropout(0.1)(x)
+    x = layers.Dense(16, activation='relu')(x)
+    x = layers.Dropout(0.1)(x)
+    outputs = layers.Dense(1, activation='relu')(x)
 
-model = keras.Model(inputs=inputs, outputs=outputs)
+    model = keras.Model(inputs=inputs, outputs=outputs)
 
-model.summary()
+    model.summary()
 
-# Train and Evaluate
+    ## Train and Evaluate
 
-model.compile(
-    loss="mean_squared_error", optimizer=Adam(lr=0.00001), metrics=["mae", r2]
-)
+    model.compile(loss='mean_squared_error',
+                  optimizer=Adam(lr=0.00001),
+                  metrics=['mae',r2])
 
 # set up a bunch of callbacks to do work during model training..
 
-checkpointer = ModelCheckpoint(
-    filepath="smile_regress.autosave.model.h5",
-    verbose=1,
-    save_weights_only=True,
-    save_best_only=True,
-)
-csv_logger = CSVLogger("smile_regress.training.log")
-reduce_lr = ReduceLROnPlateau(
-    monitor="val_loss",
-    factor=0.75,
-    patience=20,
-    verbose=1,
-    mode="auto",
-    epsilon=0.0001,
-    cooldown=3,
-    min_lr=0.000000001,
-)
-early_stop = EarlyStopping(monitor="val_loss", patience=100, verbose=1, mode="auto")
+checkpointer = ModelCheckpoint(filepath='smile_regress.autosave.model.h5', verbose=1, save_weights_only=True, save_best_only=True)
+csv_logger = CSVLogger('smile_regress.training.log')
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.75, patience=20, verbose=1, mode='auto', epsilon=0.0001, cooldown=3, min_lr=0.000000001)
+early_stop = EarlyStopping(monitor='val_loss', patience=50, verbose=1, mode='auto')
 
-history = model.fit(
-    x_train,
-    y_train,
-    batch_size=BATCH,
-    epochs=EPOCH,
-    verbose=1,
-    validation_data=(x_val, y_val),
-    callbacks=[checkpointer, csv_logger, reduce_lr, early_stop],
-)
+print ("fitting on model with train shape {} and validation shape {}".format(
+    x_train.shape, x_val.shape))
 
-model.load_weights("smile_regress.autosave.model.h5")
+
+history = model.fit(x_train, y_train,
+                    batch_size=BATCH,
+                    epochs=EPOCH,
+                    verbose=1,
+                    validation_data=(x_val, y_val),
+                    callbacks = [checkpointer,csv_logger, reduce_lr, early_stop])
+
+model.load_weights('smile_regress.autosave.model.h5')
+
+
