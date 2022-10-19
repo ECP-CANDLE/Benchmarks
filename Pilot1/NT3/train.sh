7bd3976ed101cbc418dc464bafff2f9bab0ea498
Andreas Wilke <wilke@anl.gov>
2022-10-19 12:24:35 -0500

    Added path and options to output

--- Pilot1/NT3/train.sh
+++ Pilot1/NT3/train.sh
@@ -2,3 +2,12 @@
 
 # TRAIN SH
 # IMPROVE interface to NT3 model
+
+MODEL=nt3_baseline_keras2.py
+MODEL_PATH=`find ${REPO_DIR} -name $MODEL`
+
+echo Train NT3 at ${MODEL_PATH}
+echo Options: $@
+
+python $MODEL_PATH $@
+
