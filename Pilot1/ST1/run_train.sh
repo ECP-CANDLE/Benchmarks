60882ed57180703db21f964e99a426878ec27421
brettin <brettin@anl.gov>
2022-11-04 03:54:17 +0000

    commented

--- Pilot1/ST1/run_train.sh
+++ Pilot1/ST1/run_train.sh
@@ -4,6 +4,16 @@
 # arg2 = --in_vali
 # arg3 = GPU Device
 
+# This scripts runs on 4 gpus. It uses
+# PMI RANK to identify the input files.
+
+# This script can be run to train an
+# ensemble of models of the same neural
+# network architecture and hyperparameters.
+
+# Uses the smiles_regress_transformer.py 
+# that instanciates tf.distribute.MirroredStrategy
+
 DEVICES=0,1,2,3
 PROG=smiles_regress_transformer.py
 
@@ -22,6 +32,7 @@ echo $train
 echo $val
 
 echo "current working dir: $(pwd -P)"
+exit
 mkdir -p DIR.$base
 /bin/cp -n $train DIR.$base/
 /bin/cp -n $val DIR.$base/
