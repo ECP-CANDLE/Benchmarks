411daeeca47477a57a0f9d06abc4c99176e3b1d9
Archit <akvasan2@illinois.edu>
2023-03-08 18:17:07 +0000

    added submission script for horovod implemented st1

---
+++ Pilot1/ST1/sub_hvd.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+module load conda/2022-09-08
+conda activate
+
+DATA_PATH=/grand/datascience/avasan/ST_Benchmarks/Data/1M-flatten
+
+TFIL=ml.3CLPro_7BQY_A_1_F.Orderable_zinc_db_enaHLL.sorted.4col.dd.parquet.xform-smiles.csv.reg.train
+VFIL=ml.3CLPro_7BQY_A_1_F.Orderable_zinc_db_enaHLL.sorted.4col.dd.parquet.xform-smiles.csv.reg.val
+
+EP=400
+NUMHEAD=16
+DR_TB=0.1
+DR_ff=0.1
+
+ACT=elu
+DROP=False
+LR=0.0000025
+LOSS=mean_squared_error
+HVDSWITCH=True
+
+if [$HVDSWITCH = False]; then
+    python run.py --in_train ${DATA_PATH}/${TFIL} --in_vali ${DATA_PATH}/${VFIL} --ep $EP --num_heads $NUMHEAD --DR_TB $DR_TB --DR_ff $DR_ff --activation $ACT --drop_post_MHA $DROP --lr $LR --loss_fn $LOSS --hvd_switch $HVDSWITCH
+
+else
+    NP=8
+    PPN=4
+    OUT=logfile.log
+    mpiexec --np $NP -ppn $PPN --cpu-bind verbose,list:0,1,2,3,4,5,6,7 -env NCCL_COLLNET_ENABLE=1 -env NCCL_NET_GDR_LEVEL=PHB python run.py  --in_train ${DATA_PATH}/${TFIL} --in_vali ${DATA_PATH}/${VFIL} --ep $EP --num_heads $NUMHEAD --DR_TB $DR_TB --DR_ff $DR_ff --activation $ACT --drop_post_MHA $DROP --lr $LR --loss_fn $LOSS --hvd_switch $HVDSWITCH > $OUT
+
+fi
+
